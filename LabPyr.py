e6a5d235c58bfacdf600a77ebc8ab8a34aad3f17
ComputationalAstrologer <polygraph911@gmail.com>
2018-05-11 17:44:11 -0400

    Working on Fresnel propagation...

--- LabPyr.py
+++ LabPyr.py
@@ -14,20 +14,30 @@ params is a dictionary of the basic parameters of the numerical model
 
 import numpy as np
 
-pyrparams = {}
+pyrparams = dict()  # dict of nominal optical system paramers
 pyrparams['lambda'] = 0.8 # wavelength (microns)
 pyrparams['indref'] = 1.45 # pyramid index of refraction
 pyrparams['pslope'] = 3. # slope of pyramid faces relative to horizontal (degrees)  
-pyrparams['beamd'] = 2.e4 # input beam diameter (microns)
+pyrparams['beam_diam'] = 1.e4 # input beam diameter (microns)
+pyrparams['d_e2l1'] = 1.e5 # nominal distance from entrance pupil to lens1
 pyrparams['f1'] = 1.e6 # focal length of lens #1 (focuses light on pyramid tip)
-pyrparams['f2'] = 1.e6 # focal length of lens #2 (makes 4 pupil images)
+pyrparams['d_p2l2'] = 1.e5 # distance from pyramid tip to lens 1 (corrected for pyramid glass)
+pyrparams['f2'] = 1.e5 # focal length of lens #2 (makes 4 pupil images)
 pyrparams['npup'] = 33  # number of pixels in entrace pupil
 pyrparams['npad1'] = 2048 # number of points in first FFT
 pyrparams['npad2'] = 2048 # number of points in second FFT
 
-class Pyr():
+# This simulator two dimentions perpendicular to the optical axis and is designed
+#   to model the PyWFS in the lab.
+class Pyr2D():
     def __init__(self, params=pyrparams):
         self.params = params
+        return
+
+# This simulator has only 1 dimension transverse to the optical axis.  The purpose is
+#   to test numerical resolution and code algorithms/structures.
+class Pyr1D():
+    def __init__(self, params=pyrparams):
         return
 
     # This calculates the field near the focal plane of lens1, which is near the
@@ -39,14 +49,37 @@ class Pyr():
     def Prop2FirstFocus(self, pfield, den, dfo):
         return(np.nan)
 
-def myfft(g):  # for centered arrays, custom normalization
-    if g.ndim == 1:
-        return np.fft.fftshift(np.fft.fft(np.fft.fftshift(g)))/np.sqrt(len(g))
-    elif g.ndim ==2:
-        return(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(g)))/np.sqrt(g.shape[0]*g.shape[1]))
+# 1D planar Fresnel beam prop.
+# f - complex valued field in initial plane
+# z - propagation distance
+# diam - diameter of beam
+# lam - wavelength
+#   NOTE: z, diam, lam must all be in the same units
+# npad - must be greater than len(f)
+def FresnelProp1D(f, z, diam, lam = 0.8, npad=2048):
+    fz = myzp(f, npad)
+    dx = diam/(1. + len(f)) # assumes points on f are bin centers
+    k = np.linspace(- (npad+1.)/2, (npad+1.)/2, npad)*2*np.pi/(dx*lam)
+    return( myfft(myfft(fz,-1)*np.exp(-1j*np.pi*lam*z*k*k), 1) )
+
+#the FORWARD FFT corresponds to direction = -1, the inverse is +1
+def myfft(g, direction=-1):  # for centered arrays, custom normalization
+    if direction == -1:
+        if g.ndim == 1:
+            return np.fft.fftshift(np.fft.fft(np.fft.fftshift(g)))/np.sqrt(len(g))
+        elif g.ndim ==2:
+            return(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(g)))/np.sqrt(g.shape[0]*g.shape[1]))
+        else:
+            raise Exception("Input array must be 1D or 2D.")
+    elif direction == 1:
+        if g.ndim == 1:
+            return np.fft.ifftshift(np.fft.ifft(np.fft.ifftshift(g)))
+        elif g.ndim ==2:
+            return(np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(g))))
+        else:
+            raise Exception("Input array must be 1D or 2D.")
     else:
-        raise Exception("Input array must be 1D or 2D.")
-        return(np.nan)
+        raise Exception("Invalid direction.")
 
 #  this zero pad function gives rise to purely real myfft with symm. input
 def myzp(f, npad):  # zero-pad function for pupil fields
@@ -59,7 +92,7 @@ def myzp(f, npad):  # zero-pad function for pupil fields
         if np.mod(npad, 2) == 1:
             raise Exception("npad must be even.")
         g = np.zeros(npad).astype('complex')
-        g[npad/2 - nfield/2 + 1:npad/2 + nfield/2] = f
+        g[int(npad/2) - int(nfield/2) + 1:int(npad/2) + int(nfield/2)] = f
         return(g)
     elif f.ndim == 2:
         if f.shape[0] != f.shape[1]:
@@ -72,8 +105,8 @@ def myzp(f, npad):  # zero-pad function for pupil fields
         if np.mod(npad, 2) == 1:
             raise Exception("npad must be even.")
         g = np.zeros((npad, npad)).astype('complex')
-        g[npad/2 - nfield/2 + 1:npad/2 + nfield/2,
-          npad/2 - nfield/2 + 1:npad/2 + nfield/2] = f
+        g[int(npad/2) - int(nfield/2) + 1:int(npad/2) + int(nfield/2),
+          int(npad/2) - int(nfield/2) + 1:int(npad/2) + int(nfield/2)] = f
         return(g)
     else:
         raise Exception("Input array must be 1D or 2D.")
