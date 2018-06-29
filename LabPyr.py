ad8b37dcc2df38655345373d4f685f6794318a92
ComputationalAstrologer <polygraph911@gmail.com>
2018-06-29 17:52:02 +0200

    Changed ConvFresnel1D to use new param: set_dx.  Started 2D stuff.  Simulated focusing experiment in lab.

--- LabPyr.py
+++ LabPyr.py
@@ -20,22 +20,25 @@ import matplotlib.pyplot as plt
 # dict of nominal optical system paramers
 #  units: lengths are in microns, angles are in degrees
 pyrparams = dict()
-pyrparams['wavelength'] = 0.8 # wavelength (microns)
-pyrparams['indref'] = 1.45 # pyramid index of refraction
+pyrparams['wavelength'] = 0.633 # wavelength (microns)
+pyrparams['indref'] = 1.5 # pyramid index of refraction
 pyrparams['pslope'] = 4. # slope of pyramid faces relative to horizontal (degrees)  
-pyrparams['beam_diameter'] = 1.e3 # input beam diameter (microns)
-pyrparams['D_e_2_l1'] = 20.e3 # nominal distance from entrance pupil to lens1
-pyrparams['f1'] = 50.e3 # focal length of lens #1 (focuses light on pyramid tip)
+pyrparams['beam_diameter'] = 3.e3 # input beam diameter (microns)
+pyrparams['D_e_2_l1'] = 70.e3 # nominal distance from entrance pupil to lens1 (microns)
+pyrparams['f1'] = 200.e3 # focal length of lens #1 (focuses light on pyramid tip)
 pyrparams['D_l1_2_pyr'] = 50.e3 # distance from pyramid tip to lens 1 (corrected for pyramid glass)
 pyrparams['apex_diam'] = 8 # diameter of focal spot at pyramid apex in units of lambda_over_D (set by stop)
 pyrparams['D_foc_2_l2'] = 50.e3 # distance from focus to lens #2 (includes effective OPL thru prism)
-pyrparams['f2'] = 50.e3 # focal length of lens #2 (makes 4 pupil images)
+pyrparams['f2'] = 5023.e3 # focal length of lens #2 (makes 4 pupil images)
 pyrparams['diam_lens2'] = 6.e3 # effective diameter of lens2 (set by a stop)
 pyrparams['D_l2_2_detector'] = 10.e3 # distrance from lens2 to detector
 pyrparams['detector_width'] = 6.e3 # width of detector
-pyrparams['max_chirp_step_deg'] = 30  # maximum allowed value (degrees) in chirp step for Fresnel prop
+pyrparams['max_chirp_step_deg'] = 60  # maximum allowed value (degrees) in chirp step for Fresnel prop
 pyrparams['max_lens_step_deg'] = 15  # maximum step size allowed for lens phase screen
 pyrparams['max_pyramid_step_deg'] = 10 # maximum step (degrees) allowed for pyramid phase ramp
+
+def sq(field):
+    return(np.real(field*np.conj(field)))
 
 # This simulator has only 1 dimension transverse to the optical axis.  The purpose is
 #   to test numerical resolution and code algorithms/structures.
@@ -56,32 +59,61 @@ class Pyr1D():
         #self.FocusEntrance1D()
         return
 
-    #This runs a test for bringing the entrance pupil to a focus
-    def FocusEntrance1D(self):
-        diam0 = self.params['beam_diameter']
-        foc = self.params['f1']
-        x = self.x_Start
-
-        print("initial x.shape= " + str(x.shape))
-        lfield, x = self.ApplyThinLens1D(self.field_Start, x, 0., foc, return_derivs=False)
-
-        print("post lens x.shape= " + str(x.shape))
-        
-        plt.figure()
-        plt.plot(x, np.angle(lfield)*180/np.pi,'bo-')
-
-        lambda_over_D =  self.params['wavelength']*foc/diam0
-        diam1 = 5*lambda_over_D
-        ffield, x = self.ConvFresnel1D(lfield, x, diam1, foc, return_derivs=False)
-        print("post prop x.shape= " + str(x.shape))
+    def PropBeamNoFocus1D(self):
+        diam = self.params['beam_diameter']
+        diam1 = 1.e3 + diam
+        z = self.params['D_e_2_l1'] + self.params['f1']
+        field, xout = self.ConvFresnel1D(self.field_Start, self.x_Start, diam1, z, set_dx=True, return_derivs=False)
 
         plt.figure()
-        plt.plot(x/lambda_over_D, np.real(ffield*np.conj(ffield)),'kx-')
+        intensity = np.real(field*np.conj(field))
+        plt.plot(xout/1.e3, intensity/np.max(intensity), 'x-')
+        plt.title('3 mm: unfocused beam at detector (full res)')
+        plt.xlabel('x (mm)')
+        plt.ylabel('intensity')
+
         return
+
+    #This simulates propagation of a beam to a lens and then to a plane
+    #   that is defocus from the focal length of the lens
+    #defocus - the signed distance (microns) from the focal plane
+    #   negative distances imply the plane is closer to the lens than
+    #     the focal plane.
+    def PropBeamFocus(self, defocus=0):
+        diam0 = self.params['beam_diameter'] + 1.e3
+        z = self.params['D_e_2_l1']
+        field, x = self.ConvFresnel1D(self.field_Start, self.x_Start, diam0, z, set_dx=True, return_derivs=False)
+
+        intensity = sq(field)
+        dx = x[1] - x[0]
+        plt.figure()
+        plt.plot(x, intensity/np.max(intensity),'x-')
+        plt.title('intensity at lens1, $\Delta x$ = ' + str(dx))
+        plt.xlabel('x (microns)')
+        plt.ylabel('intensity')
+
+        lens_center = 0
+        focal_length = self.params['f1']
+        field, x = self.ApplyThinLens1D(field, x, lens_center, focal_length, return_derivs=False)
+
+        diam1 = 2.e3
+        z = focal_length + defocus
+        field, x = self.ConvFresnel1D(field, x, diam1, z, set_dx=True, return_derivs=False)
+
+        intensity = sq(field)
+        dx = x[1] - x[0]
+        plt.figure()
+        plt.plot(x/5.3, intensity/np.max(intensity),'ko-')
+        plt.title('detector intensity, $\Delta x$ = ' + str(dx) + ', defocus = ' + str(defocus/1.e3) + ' mm')
+        plt.xlabel('x (pixels)')
+        plt.ylabel('intensity')
+
+        return([x, intensity/np.max(intensity)])
+
 
     # This calculates the field near the focal plane of lens1, which is near the apex of the pyramid
     # pfield - pupil plane field (assume evenly sampled)
-    def Prop1D(self):
+    def PropPyr1D(self):
 
         diam0 = self.params['beam_diameter']
         print("initial x.shape= " + str(self.x_Start.shape))
@@ -175,58 +207,146 @@ class Pyr1D():
     #1D Fresenel prop using convolution in the spatial domain
     # g - vector of complex-valued field in the inital plane
     # x - vector of coordinates in initial plane, corresponding to bin center locaitons
-    # diam1 - diameter of region to be calculated in output plane
+    # diam_out - diameter of region to be calculated in output plane
     # z - propagation distance
+    # set_dx = [True | False | dx (microns)]
+    #   True  - forces full sampling of chirp according to self.params['max_chirp_step_deg']
+    #           Note: this can lead to unacceptably large arrays.
+    #   False - zeroes the kernel beyond where self.params['max_chirp_step_deg'] 
+    #            exceedes the dx given in the x input array.  Note: the output dx will differ
+    #            slightly from dx in the x input array.
+    #...dx - sets the resolution to dx (units microns).  Note: the output dx will differ 
+    #         slightly from this value
     # return_derivs - True to return deriv. of output field WRT z
-    # maintain_dx - False allows full sampling of chirp according to self.params['max_chirp_step_deg']
-    #             - True forces zeroing the kernel beyond where self.params['max_chirp_step_deg'] 
-    #                    exceeded for the dx given in x input array
-    #     Thus, True can dramatically shrink the size of the output array
-    def ConvFresnel1D(self, g, x, diam1, z, return_derivs=False, maintain_dx=False):
+    def ConvFresnel1D(self, g, x, diam_out, z, set_dx=True, return_derivs=False):
         if g.shape != x.shape:
             raise Exception("Input field and grid must have same dimensions.")
         lam = self.params['wavelength']
-        dPhiTol_deg = self.params['max_chirp_step_deg']
-        dx, diam = self.GetDxAndDiam(x)
         nx = g.shape[0]
-        #first figure out sampling criterion for chirp
-        dx_tol = (dPhiTol_deg/180)*lam*z/(diam + diam1)  # factors of pi cancel
-        if dx > dx_tol and not maintain_dx:  # interpolate g onto finer grid
-            dx = dx_tol
-            nx = 1 + int(diam/dx)
+        dx, diam = self.GetDxAndDiam(x)
+        dPhiTol_deg = self.params['max_chirp_step_deg']
+        dx_chirp = (dPhiTol_deg/180)*lam*z/(diam + diam_out)  # sampling criterion for chirp (factors of pi cancel)
+        if set_dx == False:
+            dx_new = dx
+        elif set_dx == True:  # use chirp sampling criterion
+            dx_new = dx_chirp
+        else:  # take dx_new to be value of set_dx
+            if str(type(set_dx)) != "<class 'float'>":
+                raise Exception("ConvFresnel1D: set_dx must be a bool or a float.")
+            if set_dx <= 0:
+                raise Exception("ConvFresnel1D: numerical value of set_dx must be > 0.")
+            dx_new = set_dx
+
+        if dx != dx_new:  # interpolate g onto a grid with spacing of approx dx_new
+            nx = int(np.round(diam/dx_new))
             dx = diam/nx
             xnew = np.linspace(-diam/2 + dx/2, diam/2 - dx/2, nx)  # new grid for intial field
             interp = interp1d(x, g, 'quadratic', fill_value='extrapolate')
             g = interp(xnew)
             x = xnew
 
-        ns = int(np.round(diam + diam1)/dx)
-        s = np.linspace(-diam/2 - diam1/2 + dx/2, diam/2 + diam1/2 - dx/2, ns) # spatial grid of extended kernel
-        nt = int(np.round(diam1/dx))
-        t = np.linspace(-diam1/2 + dx/2, diam1/2 - dx/2, nt)  # output spatial grid
+        # make the kernel grid (s) match x as closely as possible
+        ns = int(np.round(diam + diam_out)/dx)  # number of points on extended kernel
+        s = np.linspace(-diam/2 - diam_out/2 + dx/2, diam/2 + diam_out/2 - dx/2, ns) # spatial grid of extended kernel
+        indices_out = np.where(np.abs(s) < diam_out/2)[0] # get the part of s within the output grid
 
-        #calculate Fresnel convoltion kernel
-        kern = np.exp(1j*np.pi*s*s/(lam*z))  # Fresnel kernel (Goodman 4-16)
-        if maintain_dx:  # where does |s| exceed the max step for this dx?
+        #Calculate Fresnel convoltion kernel, (Goodman 4-16)
+        #  Note: the factor p = 1/(lam*z) is applied later
+        #  Also note: the factor -1j*np.exp(2j*np.pi*z/lam) causes unwanted oscillations with z
+        kern = np.exp(1j*np.pi*s*s/(lam*z))  # Fresnel kernel
+        if dx > dx_chirp:  # Where does |s| exceed the max step for this dx?
             s_max = lam*z*self.params['max_chirp_step_deg']/(360*dx)
             null_ind = np.where(np.abs(s) > s_max)[0]
             kern[null_ind] = 0
-        # propagated field is given by h*p
         h = conv(kern, g, mode='same', method='fft')  # h is on the s spatial grid
-        interp = interp1d(s, h, 'quadratic')  # put h on the t spatial grid
-        h = interp(t)
 
-        #p = -1j*np.exp(2j*np.pi*z/lam)/(lam*z)  # prefactor - causes unwanted oscillations with z
         p = 1/(lam*z)
         if not return_derivs:
-            return([p*h, t])
-        #dpdz = (1j/(lam*z*z) + 2*np.pi/(lam*lam*z))*np.exp(2j*np.pi*z/lam)  # includes unwanted oscillations
+            return([p*h[indices_out], s[indices_out]])
+        #dpdz = (-1j/(lam*z*z) + 2*np.pi/(lam*lam*z))*np.exp(2j*np.pi*z/lam)  # includes unwanted oscillations
         dpdz = -1/(lam*z*z)
         dkerndz = -1j*np.pi*s*s*kern/(lam*z*z)
         dhdz = conv(dkerndz, g, mode='same', method='fft')
-        interp = interp1d(s, dhdz, 'quadratic')  # do same interpolation for derivative
-        dhdz = interp(t)
-        return([p*h, dpdz*h + p*dhdz, t])
+        s = s[indices_out]
+        h = h[indices_out]
+        dhdz = dhdz[indices_out]
+        return([p*h, dpdz*h + p*dhdz, s])
+
+    #2D Fresenel prop using convolution in the spatial domain
+    # g - matrix of complex-valued field in the inital plane
+    # x - vector of 1D coordinates in initial plane, corresponding to bin center locaitons
+    #        it is assumed that the x and y directions have the same sampling.
+    # diam_out - diameter of region to be calculated in output plane
+    # z - propagation distance
+    # set_dx = [True | False | dx (microns)]
+    #   True  - forces full sampling of chirp according to self.params['max_chirp_step_deg']
+    #           Note: this can lead to unacceptably large arrays.
+    #   False - zeroes the kernel beyond where self.params['max_chirp_step_deg'] 
+    #            exceedes the dx given in the x input array.  Note: the output dx will differ
+    #            slightly from dx in the x input array.
+    #...dx - sets the resolution to dx (units microns).  Note: the output dx will differ 
+    #         slightly from this value
+    # return_derivs - True to return deriv. of output field WRT z
+    def ConvFresnel2D(self, g, x, diam_out, z, set_dx=True, return_derivs=False):
+        if g.shape[0] != x.shape[0]:
+            raise Exception("ConvFresnel2D: input field and grid must have same sampling.")
+        if g.ndim != 2:
+            raise Exception("ConvFresnel2D: input field array must be 2D.")
+        if g.shape[0] != g.shape[1]:
+            raise Exception("ConvFresnel2D: input field array must be square.")
+
+        lam = self.params['wavelength']
+        nx = g.shape[0]
+        dx, diam = self.GetDxAndDiam(x)
+        dPhiTol_deg = self.params['max_chirp_step_deg']
+        dx_chirp = (dPhiTol_deg/180)*lam*z/(diam + diam_out)  # sampling criterion for chirp (factors of pi cancel)
+        if set_dx == False:
+            dx_new = dx
+        elif set_dx == True:  # use chirp sampling criterion
+            dx_new = dx_chirp
+        else:  # take dx_new to be value of set_dx
+            if str(type(set_dx)) != "<class 'float'>":
+                raise Exception("ConvFresnel2D: set_dx must be a bool or a float.")
+            if set_dx <= 0:
+                raise Exception("ConvFresnel2D: numerical value of set_dx must be > 0.")
+            dx_new = set_dx
+
+        if dx != dx_new:  # interpolate g onto a grid with spacing of approx dx_new
+            nx = int(np.round(diam/dx_new))
+            dx = diam/nx
+            xnew = np.linspace(-diam/2 + dx/2, diam/2 - dx/2, nx)  # new grid for intial field
+            interp = interp1d(x, g, 'quadratic', fill_value='extrapolate')
+            g = interp(xnew)
+            x = xnew
+
+        # make the kernel grid (s) match x as closely as possible
+        ns = int(np.round(diam + diam_out)/dx)  # number of points on extended kernel
+        s = np.linspace(-diam/2 - diam_out/2 + dx/2, diam/2 + diam_out/2 - dx/2, ns) # spatial grid of extended kernel
+        indices_out = np.where(np.abs(s) < diam_out/2)[0] # get the part of s within the output grid
+
+        #Calculate Fresnel convoltion kernel, (Goodman 4-16)
+        #  Note: the factor p = 1/(lam*z) is applied later
+        #  Also note: the factor -1j*np.exp(2j*np.pi*z/lam) causes unwanted oscillations with z
+        kern = np.exp(1j*np.pi*s*s/(lam*z))  # Fresnel kernel
+        if dx > dx_chirp:  # Where does |s| exceed the max step for this dx?
+            s_max = lam*z*self.params['max_chirp_step_deg']/(360*dx)
+            null_ind = np.where(np.abs(s) > s_max)[0]
+            kern[null_ind] = 0
+        h = conv(kern, g, mode='same', method='fft')  # h is on the s spatial grid
+
+        p = 1/(lam*z)
+        if not return_derivs:
+            return([p*h[indices_out], s[indices_out]])
+        #dpdz = (-1j/(lam*z*z) + 2*np.pi/(lam*lam*z))*np.exp(2j*np.pi*z/lam)  # includes unwanted oscillations
+        dpdz = -1/(lam*z*z)
+        dkerndz = -1j*np.pi*s*s*kern/(lam*z*z)
+        dhdz = conv(dkerndz, g, mode='same', method='fft')
+        s = s[indices_out]
+        h = h[indices_out]
+        dhdz = dhdz[indices_out]
+        return([p*h, dpdz*h + p*dhdz, s])
+
+
 
     #This applies the pyramid's phase ramp, applies re-gridding if phase-step is too big
     # g - input field
@@ -304,10 +424,3 @@ class Pyr1D():
         assert diam > 0
         assert dx > 0
         return([dx, diam])
-
-    #This downsamples the field to cut computation costs.  It judges how accurately the downsampled field
-    #  reproduces the original with nearest neighbor interpolation.  The downsampled field is itself
-    #  calculated with a more sophisticated interpolation.  Returns downsampled field and new grid.
-    # new_diam - diameter of resampled field.
-    def DownSampleField(self, g, x, new_diam):
-        return(np.nan)
