7d2361fa07ed04b304294289bed16b3c036f8d61
ComputationalAstrologer <polygraph911@gmail.com>
2018-07-27 17:56:07 -0400

    PyramidHeight2D works: incl. tip, tilt, (x,y) translation, rotation.  Field resampling now performed by new functions ResampleField1D, ResampleField2D.

--- LabPyr.py
+++ LabPyr.py
@@ -20,10 +20,10 @@ import matplotlib.pyplot as plt
 # dict of nominal optical system paramers
 #  units: lengths are in microns, angles are in degrees
 pyrparams = dict()
-pyrparams['wavelength'] = 0.633 # wavelength (microns)
+pyrparams['wavelength'] = 0.6328 # wavelength (microns)
 pyrparams['indref'] = 1.5 # pyramid index of refraction
 pyrparams['pyramid_slope'] = 4. # slope of pyramid faces relative to horizontal (degrees)
-pyrparams['pyramid_roofsize'] = 17.6 # length of one side of square pyramid roof (microns)
+pyrparams['pyramid_roofsize'] = 16 # length of one side of square pyramid roof (microns)
 pyrparams['beam_diameter'] = 3.e3 # input beam diameter (microns)
 pyrparams['n_starting_points'] = 150  # number of resolution elements in initital beam diameter
 pyrparams['D_e_2_l1'] = 70.e3 # nominal distance from entrance pupil to lens1 (microns)
@@ -37,7 +37,8 @@ pyrparams['D_l2_2_detector'] = 10.e3 # distrance from lens2 to detector
 pyrparams['detector_width'] = 6.e3 # width of detector
 pyrparams['max_chirp_step_deg'] = 60  # maximum allowed value (degrees) in chirp step for Fresnel prop
 pyrparams['max_lens_step_deg'] = 15  # maximum step size allowed for lens phase screen
-pyrparams['max_pyramid_step_deg'] = 10 # maximum step (degrees) allowed for pyramid phase ramp
+pyrparams['max_pyramid_phase_step'] = 10  # maximum step (degrees) allowed for pyramid phase ramp
+pyrparams['interp_style'] = 'cubic'  # type of 2d interpolator
 
 def sq(field):
     return(np.real(field*np.conj(field)))
@@ -244,26 +245,23 @@ class FresnelPyramid():
     def ApplyPyramidPhaseRamp1D(self, g, x, center=0, return_derivs=False, no_apex=False):
         if g.shape != x.shape:
             raise Exception("Input field and spatial grid must have same dimensions.")
+        if no_apex:
+            assert not return_derivs
         lam = self.params['wavelength']
         pslope = self.params['pyramid_slope']*np.pi/180
-        pyrparams['indref']
         q = self.params['indref'] - 1  # index of refraction - 1
         ramp = 2*np.pi*q*np.tan(pslope)*np.abs(x - center)/lam  # z = -|x - center|*tan(theta)
         if no_apex:
             ramp = 2*np.pi*q*np.tan(pslope)*(x - center)/lam
         phase_diff = np.abs(ramp[1] - ramp[0])*180/np.pi
-        if phase_diff > self.params['max_pyramid_step_deg']:  # if too big, we must resolve the phase ramp better
-            dx, diam = self.GetDxAndDiam(x)
-            dx *= self.params['max_pyramid_step_deg']/phase_diff
-            nx = 1 + int(np.round(diam/dx))
-            dx = diam/nx
-            xnew = np.linspace(-diam/2 + dx/2, diam/2 - dx/2, nx)
+        if phase_diff > self.params['max_pyramid_phase_step']:  # if too big, we must decrase dx
+            dx = x[1] - x[0]
+            dx_new = dx*self.params['max_pyramid_phase_step']/phase_diff
+            [g, xnew] = self.ResampleField1D(g, x, dx_new)
             ramp = 2*np.pi*q*np.tan(pslope)*np.abs(xnew - center)/lam
             if no_apex:
                 ramp = 2*np.pi*q*np.tan(pslope)*(xnew - center)/lam
-            interp = interp1d(x, g, 'quadratic', fill_value='extrapolate')
-            g = interp(xnew)
-            x = xnew
+
         phasor = np.exp(1j*ramp)
         if not return_derivs:
             return([g*phasor, x])
@@ -276,10 +274,11 @@ class FresnelPyramid():
         d_phasor = 1j*phasor*np.hstack([bb,aa])
         return([g*phasor, g*d_phasor, x])
 
-    #rotate is the offset of the rotation of the pyramid (degrees)
+    #rotate (degrees) is the offset of the rotation of the pyramid (nominal is 45 deg)
     #   Zero rotation correspons to pyramid edges at 45 degrees to the x-y coords
     #tip (degrees) corresponds to rotating the pyramid about the apex in x-z plane
     #tilt (degress)                                                      y-z
+    #   tip and tilt are relative to the pyramid axes
     def ApplyPyramidPhaseRamp2D(self, g, x, center=[0,0], rotate=0, tip=0, tilt=0, return_derivs=False):
         if g.shape[0] != x.shape[0]:
             raise Exception("ApplyPyramidPhaseRamp2D: input field and grid must have same sampling.")
@@ -289,43 +288,65 @@ class FresnelPyramid():
             raise Exception("ApplyPyramidPhaseRamp2D: input field array must be square.")
         if len(center) != 2:
             raise Exception("ApplyPyramidPhaseRamp2D: center parameter must have len=2.")
-            
+
+        lam = self.params['lambda']
+        indref = self.params['indref']
+        tslope = np.tan(self.params['pyramid_slope'])
+        dx, diam = self.GetDxAndDiam(x)
+        phase_step = 360*(indref-1)*dx*tslope/lam
+        if phase_step > self.params['max_pyramid_phase_step']:
+            dxnew = self.params['max_pyramid_phase_step']*lam/(360*tslope)
+            [g, x] = self.ResampleField2D(g, x, dxnew, kind=self.params['interp_style'])
+
         height = self.PyramidHeight2D(x, center, rotate, tip, tilt)
-        ramp = np.exp(-2j*np.pi*height*(self.params['indref'] - 1)/self.params['lambda'])
+        ramp = np.exp(-2j*np.pi*height*(indref - 1)/lam)
         g *= ramp
         if not return_derivs:
             return([g, x])
         # Unfortunately, I think finite differences is the best option for these derivatives.
         #    Make sure the steps are not to small for the delta x
-        
+
     def PyramidHeight2D(self, x, center, rotate, tip, tilt):
         #calculate the height of the pyramid face.  Rotate according to (rotate, tip, tilt) params
         tan_slope = np.tan(self.params['pyramid_slope']*np.pi/180.)
         roof = self.params['pyramid_roofsize']/2
         nx = x.shape[0]
-        [sx, sy] = np.meshgrid(x - center[0], x - center[1], indexing='xy')  # note coord shift
-        th = np.arctan2(sy,sx) + np.pi/4 + rotate*np.pi/180
-        sz = np.zeros(th.shape)  # face height (taken to be >= 0)
+        [px, py] = np.meshgrid(x - center[0], x - center[1], indexing='xy')  # note coord shift
+        xs = np.zeros(px.shape)
+        ys = np.zeros(px.shape)
+        zs = np.zeros(px.shape)  # face height (taken to be >= 0)
+        rotate = (rotate + 45)*np.pi/180
         for k in range(nx):
             for l in range(nx):
-                if (th[k,l] >= -np.pi/4) and (th[k,l] < np.pi/4):  # +x direction
-                    if np.abs(sx[k,l]) >= roof:
-                        sz[k,l] = (np.abs(sx[k,l]) - roof)*tan_slope
-                    sz[k,l] = sz[k,l]*np.cos(tip*np.pi/180) - sx[k,l]*np.sin(tip*np.pi/180)
-                elif (th[k,l] >= np.pi/4) and (th[k,l] < 3*np.pi/4):  # +y direction
-                    if np.abs(sy[k,l]) >= roof:
-                        sz[k,l] = (np.abs(sy[k,l]) - roof)*tan_slope
-                    sz[k,l] = sz[k,l]*np.cos(tilt*np.pi/180) - sy[k,l]*np.sin(tilt*np.pi/180)
-                elif (th[k,l] >= 3*np.pi/4) or (th[k,l] < -3*np.pi/4):  # -x direction
-                    if np.abs(sx[k,l]) >= roof:
-                        sz[k,l] = (np.abs(sx[k,l]) - roof)*tan_slope
-                    sz[k,l] = sz[k,l]*np.cos(tip*np.pi/180) - sx[k,l]*np.sin(tip*np.pi/180)
+                sx = px[k,l]*np.cos( rotate) + py[k,l]*np.sin(rotate)
+                sy = px[k,l]*np.sin(-rotate) + py[k,l]*np.cos(rotate)
+                th = np.arctan2(sy, sx)
+                xs[k,l] = sx
+                ys[k,l] = sy
+                if (th >= -np.pi/4) and (th < np.pi/4):  # +x direction
+                    if np.abs(sx) >= roof:
+                        zs[k,l] = (np.abs(sx) - roof)*tan_slope
+                elif (th >= np.pi/4) and (th < 3*np.pi/4):  # +y direction
+                    if np.abs(sy) >= roof:
+                        zs[k,l] = (np.abs(sy) - roof)*tan_slope
+                elif (th >= 3*np.pi/4) or (th < -3*np.pi/4):  # -x direction
+                    if np.abs(sx) >= roof:
+                        zs[k,l] = (np.abs(sx) - roof)*tan_slope
                 else:  # -y direction
-                    if np.abs(sy[k,l]) >= roof:
-                        sz[k,l] = (np.abs(sy[k,l]) - roof)*tan_slope
-                    sz[k,l] = sz[k,l]*np.cos(tilt*np.pi/180) - sy[k,l]*np.sin(tilt*np.pi/180)
-                
-        return()
+                    if np.abs(sy) >= roof:
+                        zs[k,l] = (np.abs(sy) - roof)*tan_slope
+        # to apply tip, rotate about y-axis.  for tilt, rotate about x-axis
+        if tip != 0:
+            t = tip*np.pi/180 
+            for k in range(nx):
+                for l in range(nx):
+                    xs[k,l], zs[k,l] = xs[k,l]*np.cos(t) + zs[k,l]*np.sin(t), xs[k,l]*np.sin(-t) + zs[k,l]*np.cos(t)
+        if tilt != 0:
+            t = tilt*np.pi/180
+            for k in range(nx):
+                for l in range(nx):
+                    ys[k,l], zs[k,l] = ys[k,l]*np.cos(t) + zs[k,l]*np.sin(t), ys[k,l]*np.sin(-t) + zs[k,l]*np.cos(t)
+        return(zs)
 
 
     #1D Fresenel prop using convolution in the spatial domain
@@ -346,7 +367,6 @@ class FresnelPyramid():
         if g.shape != x.shape:
             raise Exception("Input field and grid must have same dimensions.")
         lam = self.params['wavelength']
-        nx = g.shape[0]
         dx, diam = self.GetDxAndDiam(x)
         dPhiTol_deg = self.params['max_chirp_step_deg']
         dx_chirp = (dPhiTol_deg/180)*lam*z/(diam + diam_out)  # sampling criterion for chirp (factors of pi cancel)
@@ -361,13 +381,9 @@ class FresnelPyramid():
                 raise Exception("ConvFresnel1D: numerical value of set_dx must be > 0.")
             dx_new = set_dx
 
-        if dx != dx_new:  # interpolate g onto a grid with spacing of approx dx_new
-            nx = int(np.round(diam/dx_new))
-            dx = diam/nx
-            xnew = np.linspace(-diam/2 + dx/2, diam/2 - dx/2, nx)  # new grid for intial field
-            interp = interp1d(x, g, 'quadratic', fill_value='extrapolate')
-            g = interp(xnew)
-            x = xnew
+        if dx != dx_new:  # interpolate g onto a grid with spacing of (approx) dx_new
+            [g, x] = self.ResampleField1D(g, x, dx_new)
+            dx = x[1] - x[0]
 
         # make the kernel grid (s) match x as closely as possible
         ns = int(np.round(diam + diam_out)/dx)  # number of points on extended kernel
@@ -422,7 +438,6 @@ class FresnelPyramid():
             raise Exception("ConvFresnel2D: input field array must be square.")
 
         lam = self.params['wavelength']
-        nx = g.shape[0]
         dx, diam = self.GetDxAndDiam(x)
         dPhiTol_deg = self.params['max_chirp_step_deg']
         dx_chirp = (dPhiTol_deg/180)*lam*z/(diam + diam_out)  # sampling criterion for chirp (factors of pi cancel)
@@ -438,14 +453,8 @@ class FresnelPyramid():
             dx_new = set_dx
 
         if dx != dx_new:  # interpolate g onto a grid with spacing of approx dx_new
-            nx = int(np.round(diam/dx_new))
-            dx = diam/nx
-            xnew = np.linspace(-diam/2 + dx/2, diam/2 - dx/2, nx)  # new grid for intial field
-            #interp2d doesn't like complex numbers.   How stupid.
-            interp_real = interp2d(x, x, np.real(g), 'cubic', fill_value=None)
-            interp_imag = interp2d(x, x, np.imag(g), 'cubic', fill_value=None)
-            g = interp_real(xnew, xnew) + 1j*interp_imag(xnew, xnew)
-            x = xnew
+            [g, x] = self.ResampleField2D(g, x, dx_new, kind=self.params['interp_style'])
+            dx = x[1] - x[0]
 
         # make the kernel grid (s) match x as closely as possible
         ns = int(np.round(diam + diam_out)/dx)  # number of points on extended kernel
@@ -489,11 +498,7 @@ class FresnelPyramid():
         max_step = self.params['max_lens_step_deg']*np.pi/180
         dx_tol = max_step*lam*fl/(2*np.pi*(diam/2 + np.abs(center)))
         if dx > dx_tol:  # interpolate onto higher resolution grid
-            nx = int(np.round(diam/dx_tol))
-            dx = diam/nx
-            xnew = np.linspace(-diam/2 + dx/2, diam/2 + dx/2, nx)
-            interp = interp1d(x, g, 'quadratic', fill_value='extrapolate')
-            g = interp(xnew)
+            [g, xnew] = self.ResampleField1D(g, x, dx_tol)
         else:
             xnew = x
         h = g*np.exp(-1j*np.pi*(xnew - center)*(xnew - center)/(fl*lam))
@@ -518,26 +523,58 @@ class FresnelPyramid():
         max_step = self.params['max_lens_step_deg']*np.pi/180
         dx_tol = max_step*lam*fl/(2*np.pi*(diam/2 + np.sqrt(center[0]**2 + center[1]**2)))
         if dx > dx_tol:  # interpolate onto higher resolution grid
-            nx = int(np.round(diam/dx_tol))
-            dx = diam/nx
-            xnew = np.linspace(-diam/2 + dx/2, diam/2 + dx/2, nx)
-            #interp2d doesn't like complex numbers.   How stupid.
-            interp_real = interp2d(x, x, np.real(g), 'cubic', fill_value=None)
-            interp_imag = interp2d(x, x, np.imag(g), 'cubic', fill_value=None)
-            g = interp_real(xnew, xnew) + 1j*interp_imag(xnew, xnew)
-        else:
-            xnew = x
+            [g, x] = self.ResampleField2D(g, x, dx_tol, kind=self.params['interp_style'])
 
-        [sx, sy] = np.meshgrid(xnew,xnew)
+        [sx, sy] = np.meshgrid(x, x, indexing='xy')
         sx -= center[0]
         sy -= center[1]
         h = g*np.exp(-1j*np.pi*(sx*sx + sy*sy)/(fl*lam))
         if not return_derivs:
-            return([h, xnew])
+            return([h, x])
         dhdc0 = 2j*np.pi*sx*h/(lam*fl)
         dhdc1 = 2j*np.pi*sy*h/(lam*fl)
-        return([h, [dhdc0, dhdc1], xnew])
+        return([h, [dhdc0, dhdc1], x])
 
+    #Resample the field to achieve a target spatial resolution given by dx_new
+    #  Returns resampled field and new x
+    #g - input field
+    #x - initial 1D spatial grid
+    #dx_new (microns) - desired spatial resolution
+    #kind, fill_value - keywords passed to scipy.interpolate.interp2d
+    def ResampleField2D(self, g, x, dx_new, kind='cubic', fill_value=None):
+        if g.shape[0] != x.shape[0]:
+            raise Exception("ResampleField2D: input field and grid must have same sampling.")
+        if g.ndim != 2:
+            raise Exception("ResampleField2D: input field array must be 2D.")
+        if g.shape[0] != g.shape[1]:
+            raise Exception("ResampleField2D: input field array must be square.")
+
+        dx, diam = self.GetDxAndDiam(x)
+        nx = int(np.round(diam/dx_new))
+        dxnew = diam/nx
+        xnew = np.linspace(-diam/2 + dxnew/2, diam/2 - dxnew/2, nx)
+        #interp2d doesn't like complex number.  So stupid.
+        interp_real = interp2d(x, x, np.real(g), kind=kind, fill_value=fill_value)
+        interp_imag = interp2d(x, x, np.imag(g), kind=kind, fill_value=fill_value)
+        g = interp_real(xnew, xnew) + 1j*interp_imag(xnew, xnew)
+        return([g, xnew])
+
+    #Resample the field to achieve a target spatial resolution given by dx_new
+    #  Returns resampled field and new x
+    #g - input field
+    #x - initial 1D spatial grid
+    #dx_new (microns) - desired spatial resolution
+    #kind, fill_value - keywords passed to scipy.interpolate.interp1d
+    def ResampleField1D(self, g, x, dx_new, kind='quadratic', fill_value='extrapolate'):
+        if g.shape != x.shape:
+            raise Exception("ResampleField1D: input field and x must have the same shape.")
+        dx, diam = self.DxAndDiam(x)
+        nx = int(np.round(diam/dx_new))
+        dxnew = diam/nx
+        xnew = np.linspace(-diam/2 + dxnew/2, diam/2 - dxnew/2, nx)
+        interp = interp1d(x, g, kind=kind, fill_value=fill_value)
+        g = interp(xnew)
+        return([g, xnew])
 
     # assuming a regular spaced grid with values at bin centers,
     #  get spacing and diameter from spatial grid x
