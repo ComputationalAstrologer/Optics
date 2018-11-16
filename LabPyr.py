9917b91da8bf82686c23900e758be3d9c65f41a5
ComputationalAstrologer <polygraph911@gmail.com>
2018-11-16 17:55:18 -0500

    The module LabPyr.py now contains two classes for the various optical models.  The class called WorkingOpticalModels contains stuff that is debugged and works properly.  The class called NotWorkingOpticalModels may be hazardous to your health.

--- LabPyr.py
+++ LabPyr.py
@@ -24,7 +24,7 @@ pyrparams = dict()
 pyrparams['beam_diameter'] = 1.e3 # input beam diameter (microns)
 pyrparams['wavelength'] = 0.6328 # wavelength (microns)
 pyrparams['indref'] = 1.5 # pyramid index of refraction
-pyrparams['pyramid_slope_deg'] = 3# 10.5  # slope of pyramid faces relative to horizontal (degrees)
+pyrparams['pyramid_slope_deg'] = 1.5# 10.5  # slope of pyramid faces relative to horizontal (degrees)
 pyrparams['pyramid_roofsize'] = 16 # length of one side of square pyramid roof (microns)
 pyrparams['pyramid_height'] = 1.e4 # (microns)  height of pyramid
 pyrparams['n_starting_points'] = 150  # number of resolution elements in initital beam diameter
@@ -49,7 +49,8 @@ pyrparams['interp_style'] = 'cubic'  # type of 2d interpolator
 def sq(field):
     return(np.real(field*np.conj(field)))
 
-class OpticalModels():
+#This class is for the models that aren't working yet
+class NotWorkingOpticalModels():
     def __init__(self, params=pyrparams):
 
         self.params = params
@@ -71,47 +72,6 @@ class OpticalModels():
 
         return
 
-    def PropBeamNoFocus(self, PhaseScreenTest=False):
-        FO = FourierOptics.FourierOptics(pyrparams)
-        diam = self.params['beam_diameter']
-        diam1 = 1.e3 + diam
-        z = self.params['D_e_2_l1'] + self.params['D_l1_2_detector']
-        x = 1.*self.x_Start
-        field1D = 1.*self.field_Start1D
-
-        if PhaseScreenTest:
-            diam1 += 1.5e3
-            aa = 1.e3/z
-            norm = np.array([aa, np.sqrt(1 - aa*aa)])
-            field1D, xout = FO.ApplyPlanarPhaseScreen1D(field1D, x, norm, nA=1, nB=2)
-            #undo most of phase screen by simulating a wedge
-            field1D = FO.ApplySnellAtPlane1D(field1D, xout, [0,1], nA=2, nB=1)
-
-        field1D, xout = FO.ConvFresnel1D(field1D, x, diam1, z, set_dx=True, return_derivs=False)
-
-        plt.figure()
-        intensity = np.real(field1D*np.conj(field1D))
-        plt.plot(xout/1.e3, intensity/np.max(intensity), 'x-')
-        plt.title('unfocused beam at detector (full res)')
-        plt.xlabel('x (mm)')
-        plt.ylabel('intensity')
-
-        x = 1.*self.x_Start
-        field2D = 1.*self.field_Start2D
-        if PhaseScreenTest:
-            aa = 1.e3/z
-            norm = np.array([0, aa, np.sqrt(1 - aa*aa)])  # vertical displacement
-            field2D, xout = FO.ApplyPlanarPhaseScreen2D(field2D, x, norm, nA=1, nB=2)
-
-        field2D, xout = FO.ConvFresnel2D(field2D, xout, diam1, z, set_dx=True, return_derivs=False)
-        br2D = sq(field2D)
-        br2D /= np.max(br2D)
-
-        xoutmm = xout/1.e3
-        plt.figure()
-        plt.imshow(br2D, extent=[xoutmm[0], xoutmm[-1], xoutmm[0], xoutmm[-1]]); plt.colorbar();
-
-        return
 
     #This focuses a beam on a wedge.  When the angle tip_deg == 0, the normal to the
     #  exit surface is in the z-direction.
@@ -158,6 +118,162 @@ class OpticalModels():
         spot_size = diam + 2*focal_length*(ind_ref - 1)*WedgeAngle
         field1d, x1d = FO.ConvFresnel1D(field1d, x1d, spot_size, d1, index_of_refraction=1,
                                         set_dx=True, return_derivs=False)
+        return
+
+    # This calculates the field near the focal plane of lens1, which is near the apex of the pyramid
+    # pfield - pupil plane field (assume evenly sampled)
+    def PropPyr1D(self):
+        FO = FourierOptics.FourierOptics(pyrparams)
+        diam0 = self.params['beam_diameter']
+        print("initial x.shape= " + str(self.x_Start.shape))
+
+        # Fresnel prop to lens #1
+        z = self.params['D_e_2_l1']
+        [f, df_dparam, x] = FO.ConvFresnel1D(self.field_Start1D, self.x_Start,1.2*diam0, z, return_derivs=True)
+        self.grads['D_e_2_l1'] = df_dparam
+
+        print("after prop to lens1 x.shape= " + str(x.shape))
+        plt.figure(); plt.plot(x, np.real(f*np.conj(f))); 
+        plt.title('Intensity impinging on Lens #1'); plt.xlabel("distance ($\mu$m)")
+
+        # apply lens #1
+        foc = self.params['f1']
+        for key in self.grads:  # update gradients
+            self.grads[key], _ = FO.ApplyThinLens1D(self.grads[key], x, 0, foc, return_derivs=False)
+        f, df_dparam, x = FO.ApplyThinLens1D(f, x, 0, foc, return_derivs=True) # update field
+        self.grads['l1_center'] = df_dparam
+
+        print("post lens1 x.shape= " + str(x.shape))
+
+        # Fresnel prop to focus
+        lambda_over_D =  self.params['wavelength']*foc/diam0  
+        diam1 = self.params['apex_diam']*lambda_over_D
+        for key in self.grads:  # propagate gradients
+            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam1, foc, return_derivs=False)
+        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam1, foc, return_derivs=True) # prop field
+        self.Focal_field = f*1.0 # ensure deep copy
+        self.grads['D_l1_2_apex'] = df_dparam
+
+        print("after prop to focus x.shape= " + str(x.shape))
+        plt.figure(); plt.plot(x, np.real(f*np.conj(f)));
+        plt.title("Intensity at focus (pyramid apex)"); plt.xlabel("distance ($\mu$m)")
+
+        # Apply phase ramp caused by pyramid
+        for key in self.grads:
+            self.grads[key], _ = FO.ApplyPyramidPhaseRamp1D(self.grads[key], x, 0., return_derivs=False, no_apex=False)
+        f, df_dparam, x = FO.ApplyPyramidPhaseRamp1D(f, x, 0, return_derivs=True, no_apex=False)
+        self.grads['pyramid_center'] = df_dparam
+
+        print("after pyramid x.shape= " + str(x.shape))
+
+        # Fresnel prop to lens #2
+        z = self.params['D_foc_2_l2']
+        diam2 = self.params['diam_lens2']
+        for key in self.grads:  # propagate gradients
+            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam2, z, return_derivs=False)
+        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=True)
+        self.grads['D_foc_2_l2'] = df_dparam
+
+        plt.figure(); plt.plot(x, np.real(f*np.conj(f)));
+        plt.title('Intensity impinging on lens2'); plt.xlabel("distance ($\mu$m)")
+        print("after prop to lens2 x.shape= " + str(x.shape))
+
+        #apply lens #2
+        foc = self.params['f2']
+        for key in self.grads:  # update gradients
+            self.grads[key], _ = FO.ApplyThinLens1D(self.grads[key], x, 0, foc, return_derivs=False)
+        f, df_dparam, x = FO.ApplyThinLens1D(f, x, 0, foc, return_derivs=True) # update field
+        self.grads['l2_center'] = df_dparam
+
+        print("post lens2 x.shape= " + str(x.shape))
+
+        # Fresnel prop to detector
+        z = self.params['D_l2_2_detector']
+        diam2 = self.params['detector_width']
+
+        if False:  #  compare full result to maintain_dx results
+            # get full result
+            fc, xc = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=False, maintain_dx=False)
+            plt.figure(); plt.plot(xc, np.real(fc*np.conj(fc))); plt.title('Intensity at detector, full res'); plt.xlabel("distance ($\mu$m)")
+            print("at detector xc.shape= " + str(xc.shape))
+            # get maintain_dx result
+            fc, xc = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=False, maintain_dx=True)
+            plt.figure(); plt.plot(xc, np.real(fc*np.conj(fc))); plt.title('Intensity at detector, maintain_dx'); plt.xlabel("distance ($\mu$m)")
+            print("at detector xc.shape= " + str(xc.shape))
+
+        # maintain_dx=True worked well!
+        for key in self.grads:  # propagate gradients
+            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam2, z, return_derivs=False, maintain_dx=True)
+        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=True, maintain_dx=True)
+        self.grads['D_l2_2_detector'] = df_dparam
+
+        print("at detector x.shape= " + str(x.shape))
+        plt.figure(); plt.plot(x, np.real(f*np.conj(f))); plt.title('Intensity at detector'); plt.xlabel("distance ($\mu$m)")
+
+        return
+
+#This class is for the models that work.
+class WorkingOpticalModels():
+    def __init__(self, params=pyrparams):
+
+        self.params = params
+        self.grads = dict()  # dictionary of gradients
+        nx = self.params['n_starting_points']
+        self.field_Start1D = np.ones(nx)  # initial pupil field
+
+        # make a circular input field
+        s = np.linspace(-1 + .5/nx, 1 - .5/nx, nx)
+        [sx, sy] = np.meshgrid(s, s, indexing='xy')
+        cr = np.where(sx*sx + sy*sy > 1)
+        f = np.ones((nx,nx))
+        f[cr[0],cr[1]] = 0.
+        
+        self.field_Start2D = f
+        diam0 = self.params['beam_diameter']
+        dx = diam0/nx
+        self.x_Start = np.linspace(-diam0/2 + dx/2, diam0/2 - dx/2, nx)  # initial spatial grid
+
+        return
+
+    def PropBeamNoFocus(self, PhaseScreenTest=False):
+        FO = FourierOptics.FourierOptics(pyrparams)
+        diam = self.params['beam_diameter']
+        diam1 = 1.e3 + diam
+        z = self.params['D_e_2_l1'] + self.params['D_l1_2_detector']
+        x1d = self.x_Start
+        field1D = 1.*self.field_Start1D
+
+        if PhaseScreenTest:
+            diam1 += 1.5e3
+            aa = 1.e3/z
+            norm = np.array([aa, np.sqrt(1 - aa*aa)])
+            field1D, x1d = FO.ApplyPlanarPhaseScreen1D(field1D, x1d, norm, nA=1, nB=2)
+            #undo most of phase screen by simulating a wedge
+            field1D = FO.ApplySnellAtPlane1D(field1D, x1d, [0,1], nA=2, nB=1)
+
+        field1D, x1d = FO.ConvFresnel1D(field1D, x1d, diam1, z, set_dx=True, return_derivs=False)
+
+        plt.figure()
+        intensity = np.real(field1D*np.conj(field1D))
+        plt.plot(x1d/1.e3, intensity/np.max(intensity), 'x-')
+        plt.title('unfocused beam at detector (full res)')
+        plt.xlabel('x (mm)')
+        plt.ylabel('intensity')
+
+        x2d = 1.*self.x_Start
+        field2D = 1.*self.field_Start2D
+        if PhaseScreenTest:
+            aa = 1.e3/z
+            norm = np.array([0, aa, np.sqrt(1 - aa*aa)])  # vertical displacement
+            field2D, xout = FO.ApplyPlanarPhaseScreen2D(field2D, x2d, norm, nA=1, nB=2)
+
+        field2D, xout = FO.ConvFresnel2D(field2D, x2d, diam1, z, set_dx=True, return_derivs=False)
+        br2D = sq(field2D)
+        br2D /= np.max(br2D)
+
+        xoutmm = x2d/1.e3
+        plt.figure()
+        plt.imshow(br2D, extent=[xoutmm[0], xoutmm[-1], xoutmm[0], xoutmm[-1]]); plt.colorbar();
 
         return
 
@@ -219,7 +335,7 @@ class OpticalModels():
 
 
     #This simulates an f4 optical system, in which (image distance)=(object distance)=2f
-    def PropF4(self, include_pyramid=False):
+    def PropF4(self):
         FO = FourierOptics.FourierOptics(pyrparams)
         obd = self.params['D_e_2_l1'] # nominal distance from entrance pupil to lens1 (microns)
         imd = self.params['D_l1_2_detector']
@@ -279,13 +395,21 @@ class OpticalModels():
 
         return
 
-    #This simulates an f4 optical system, in which (image distance)=(object distance)=2f,
+    #This simulates an f4 (single lens) system, in which (image distance)=(object distance)=2f,
     #   except now there is an pyramid placed at a distance f from the lens
     #center, rotate, tip, tilt are passed to FO.ApplyPyramidPhaseRamp2D
-    def PropF4Pyramid(self, fig_list=None, pyr_dist_error=0, center=[0,0], rotate=0, tip=0, tilt=0):
+    #fig_list is a list contianing one or more of the following strings:
+    #   ['lens', 'pyr_apex', 'pyr_exit', 'detector']
+    #pyr_dist_error (microns) is the distance of the pyramid apex to the focal plane
+    #   of the lens.  Positive means it's too far away
+    #center [microns, microns] is error in the [x,y] position of the pyramid apex
+    #rotate (degrees) controls the rotation of the pyramid about its principle axis
+    def PropF4Pyramid(self, fig_list=None, pyr_dist_error=0, center=[0,0], rotate=0):
         if len(center) != 2:
             raise Exception("PropF4Pyramid: center parameter must have len=2.")
         FO = FourierOptics.FourierOptics(pyrparams)
+        tip = 0 #set to 0 b/c that feature doesn't work
+        tilt = 0 #set to -0- b/c that feature does not work
         obd = self.params['D_e_2_l1'] # nominal distance from entrance pupil to lens1 (microns)
         focal_length = self.params['f1'] # focal length of lens #1 (focuses light on pyramid tip)
         d2pyr = self.params['D_l1_2_pyr'] + pyr_dist_error # distance from lens1 to pyramid tip
@@ -411,99 +535,5 @@ class OpticalModels():
             plt.title('intensity at detector, $\Delta x$ = ' + str(dx))
             plt.xlabel('x (microns)')
             plt.ylabel('y (microns)')
-
-        return
-
-
-
-    # This calculates the field near the focal plane of lens1, which is near the apex of the pyramid
-    # pfield - pupil plane field (assume evenly sampled)
-    def PropPyr1D(self):
-        FO = FourierOptics.FourierOptics(pyrparams)
-        diam0 = self.params['beam_diameter']
-        print("initial x.shape= " + str(self.x_Start.shape))
-
-        # Fresnel prop to lens #1
-        z = self.params['D_e_2_l1']
-        [f, df_dparam, x] = FO.ConvFresnel1D(self.field_Start1D, self.x_Start,1.2*diam0, z, return_derivs=True)
-        self.grads['D_e_2_l1'] = df_dparam
-
-        print("after prop to lens1 x.shape= " + str(x.shape))
-        plt.figure(); plt.plot(x, np.real(f*np.conj(f))); 
-        plt.title('Intensity impinging on Lens #1'); plt.xlabel("distance ($\mu$m)")
-
-        # apply lens #1
-        foc = self.params['f1']
-        for key in self.grads:  # update gradients
-            self.grads[key], _ = FO.ApplyThinLens1D(self.grads[key], x, 0, foc, return_derivs=False)
-        f, df_dparam, x = FO.ApplyThinLens1D(f, x, 0, foc, return_derivs=True) # update field
-        self.grads['l1_center'] = df_dparam
-
-        print("post lens1 x.shape= " + str(x.shape))
-
-        # Fresnel prop to focus
-        lambda_over_D =  self.params['wavelength']*foc/diam0  
-        diam1 = self.params['apex_diam']*lambda_over_D
-        for key in self.grads:  # propagate gradients
-            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam1, foc, return_derivs=False)
-        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam1, foc, return_derivs=True) # prop field
-        self.Focal_field = f*1.0 # ensure deep copy
-        self.grads['D_l1_2_apex'] = df_dparam
-
-        print("after prop to focus x.shape= " + str(x.shape))
-        plt.figure(); plt.plot(x, np.real(f*np.conj(f)));
-        plt.title("Intensity at focus (pyramid apex)"); plt.xlabel("distance ($\mu$m)")
-
-        # Apply phase ramp caused by pyramid
-        for key in self.grads:
-            self.grads[key], _ = FO.ApplyPyramidPhaseRamp1D(self.grads[key], x, 0., return_derivs=False, no_apex=False)
-        f, df_dparam, x = FO.ApplyPyramidPhaseRamp1D(f, x, 0, return_derivs=True, no_apex=False)
-        self.grads['pyramid_center'] = df_dparam
-
-        print("after pyramid x.shape= " + str(x.shape))
-
-        # Fresnel prop to lens #2
-        z = self.params['D_foc_2_l2']
-        diam2 = self.params['diam_lens2']
-        for key in self.grads:  # propagate gradients
-            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam2, z, return_derivs=False)
-        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=True)
-        self.grads['D_foc_2_l2'] = df_dparam
-
-        plt.figure(); plt.plot(x, np.real(f*np.conj(f)));
-        plt.title('Intensity impinging on lens2'); plt.xlabel("distance ($\mu$m)")
-        print("after prop to lens2 x.shape= " + str(x.shape))
-
-        #apply lens #2
-        foc = self.params['f2']
-        for key in self.grads:  # update gradients
-            self.grads[key], _ = FO.ApplyThinLens1D(self.grads[key], x, 0, foc, return_derivs=False)
-        f, df_dparam, x = FO.ApplyThinLens1D(f, x, 0, foc, return_derivs=True) # update field
-        self.grads['l2_center'] = df_dparam
-
-        print("post lens2 x.shape= " + str(x.shape))
-
-        # Fresnel prop to detector
-        z = self.params['D_l2_2_detector']
-        diam2 = self.params['detector_width']
-
-        if False:  #  compare full result to maintain_dx results
-            # get full result
-            fc, xc = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=False, maintain_dx=False)
-            plt.figure(); plt.plot(xc, np.real(fc*np.conj(fc))); plt.title('Intensity at detector, full res'); plt.xlabel("distance ($\mu$m)")
-            print("at detector xc.shape= " + str(xc.shape))
-            # get maintain_dx result
-            fc, xc = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=False, maintain_dx=True)
-            plt.figure(); plt.plot(xc, np.real(fc*np.conj(fc))); plt.title('Intensity at detector, maintain_dx'); plt.xlabel("distance ($\mu$m)")
-            print("at detector xc.shape= " + str(xc.shape))
-
-        # maintain_dx=True worked well!
-        for key in self.grads:  # propagate gradients
-            self.grads[key], _ = FO.ConvFresnel1D(self.grads[key], x, diam2, z, return_derivs=False, maintain_dx=True)
-        f, df_dparam, x = FO.ConvFresnel1D(f, x, diam2, z, return_derivs=True, maintain_dx=True)
-        self.grads['D_l2_2_detector'] = df_dparam
-
-        print("at detector x.shape= " + str(x.shape))
-        plt.figure(); plt.plot(x, np.real(f*np.conj(f))); plt.title('Intensity at detector'); plt.xlabel("distance ($\mu$m)")
 
         return
