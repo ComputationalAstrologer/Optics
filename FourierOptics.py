ee592a139500d6612fe365971c8d9c5113d651f3
ComputationalAstrologer <polygraph911@gmail.com>
2020-08-10 11:59:14 -0400

    3SidedPyramidHeight is now NSidedPyramidHeight.  This makes an N-sided pyramid with arbitrary deviations of the normal vector of each face.  The code is short and simple :)

--- FourierOptics.py
+++ FourierOptics.py
@@ -418,33 +418,39 @@ class FourierOptics():
         # Unfortunately, I think finite differences is the best option for these derivatives.
         #    Make sure the steps are not to small for the delta x
 
-    #AlphaVec (degrees) is the vector of angles relative to the z=0 plane of each face (len=3)
-    #FaceCenterAngles (degrees) is the azimuthal angle (about the z-axis) of face centers.
-    #rot0 (degrees) is constant that gets added to all 3 FaceCenterAngles
-    def ThreeSidedPyramidHeight(self, x, AlphaVec=None, FaceCenterAngles=None, rot0=60.):
-        if FaceCenterAngles is None:
-            FaceCenterAngles = np.array([0.,120.,240.])
+    #SlopeDeviations (degrees) is the vector of deviations of the angles relative to the z=0 plane of each face (len=N)
+    #Nominal slope (degrees, > 0) is the nominal slope of the pyramid faces relative to the z=0 plane
+    #N is the number of sides of the pyramid
+    #FaceCenterAngleDeviations (degrees) is the deviation in azimuthal angles (about the z-axis) of the face centers.
+    #rot0 (degrees) is constant that gets added to all N FaceCenterAngles
+    def NSidedPyramidHeight(self, x, SlopeDeviations=None, FaceCenterAngleDeviations=None, N=4, NominalSlope=None, rot0=None):
+        if rot0 is None: rot0 = 180./N
+        if FaceCenterAngleDeviations is None:
+            FaceCenterAngleDeviations = np.zeros((N,))
         else:
-            FaceCenterAngles = np.array(FaceCenterAngles)
-            assert FaceCenterAngles.shape == (3,)
-        if AlphaVec is None:
-            AlphaVec = self.params['pyramid_slope_deg']*np.array([1.,1.,1.])
-        else:
-            AlphaVec = np.array(AlphaVec)
-            assert AlphaVec.shape == (3,)
-            assert all(AlphaVec > 0)
-        FaceCenterAngles += rot0
-        AlphaVec *= np.pi/180.
+            FaceCenterAngleDeviations = np.array(FaceCenterAngleDeviations)
+            assert FaceCenterAngleDeviations.shape == (N,)
+        if NominalSlope is None:
+            NominalSlope = self.params['pyramid_slope_deg']
+        assert np.isscalar(NominalSlope)
+        assert NominalSlope > 0.
+        if SlopeDeviations is None:
+            SlopeDeviations = np.zeros((N,))
+        SlopeDeviations = np.array(SlopeDeviations)
+        assert SlopeDeviations.shape == (N,)
+        FaceCenterAngles = np.linspace(0., 360*(N-1)/N, N)
+        FaceCenterAngles += FaceCenterAngleDeviations + rot0
+        AlphaVec = (NominalSlope*np.ones((N,)) + SlopeDeviations)*np.pi/180.
 
-        normvec = np.zeros((3,3))  # each col is normal vector
-        for k in range(3):
+        normvec = np.zeros((3,N))  # each col is normal vector
+        for k in range(N):
             normvec[1,k] = np.sin(AlphaVec[k])
             normvec[2,k] = np.cos(AlphaVec[k])
             normvec[:,k] = self.RotationMatrix(FaceCenterAngles[k], axis='z').dot(normvec[:,k])
 
         [px, py] = np.meshgrid(x , x , indexing='xy')
-        z = np.zeros((x.shape[0], x.shape[0], 3))  # contains the heights of all 3 planes
-        for k in range(3):
+        z = np.zeros((x.shape[0], x.shape[0], N))  # contains the heights of all 3 planes
+        for k in range(N):
             z[:, :, k] = (normvec[0,k]*px + normvec[1,k]*py)/normvec[2,k]
         zs = np.min(z, axis=2)
         return(zs)
