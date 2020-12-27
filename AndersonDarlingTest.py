ebfc72973ccc224841cf4357bc9a6e23c65056be
ComputationalAstrologer <polygraph911@gmail.com>
2020-12-27 16:08:37 -0500

    updated test include cases 0, 1 and 2.

--- AndersonDarlingTest.py
+++ AndersonDarlingTest.py
@@ -5,9 +5,12 @@ Created on Fri Dec 18 17:52:13 2020
 @author: rfrazin
 
 Anderson-Darling Test for univariate normality.
-See the wikipedia article.
+See the Wikipedia article.
 
-This implements the single sample test for case 3 (i.e., unknown mean and var).
+This implements the single sample test.
+The default is case 3 (i.e., unknown mean and var).
+For cases 0, 1 and 2, in which the mean and/or var are known, simply set
+   mu and/or var to their known values.
 Note that the critical values of the test statistic (called A2 here) have
 only a very weak dependence on the number of samples, making it particularly
 good for non-independent samples for which the degrees of freedom are hard to
@@ -21,12 +24,14 @@ import numpy as np
 from scipy.stats import norm
 
 #x - is an array of samples from a univariate process
-def ADtest(x):
+def ADtest(x, mu=None, var=None):
     assert x.ndim == 1
     u = np.sort(x)
     n = len(u)
-    mu = np.mean(u)
-    var = np.var(u, ddof=1)
+    if mu is None: # (cases 0 and 2)
+        mu = np.mean(u)
+    if var is None: # (cases 0 and 1)
+        var = np.var(u, ddof=1)
     y = (u - mu)/np.sqrt(var)
     s = 0.
     for k in range(n):
