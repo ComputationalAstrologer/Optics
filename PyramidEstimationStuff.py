1a13107441e66a78e8c2af04ebb14fe18f97b6b3
ComputationalAstrologer <polygraph911@gmail.com>
2021-01-27 15:37:21 -0500

    ...

--- PyramidEstimationStuff.py
+++ PyramidEstimationStuff.py
@@ -25,16 +25,16 @@ parms['pyramid_slope_deg'] = 0.4 # slope of pyramid faces relative to horizontal
 parms['n_starting_points'] = szp  # number of resolution elements in initital beam diameter
 parms['f1'] = 40.e3 # focal length of lens #1 (focuses light on pyramid tip)
 parms['D_e_to_l1'] = 2*parms['f1'] # nominal distance from entrance pupil to lens1 (microns)
-parms['lens1_fill_diameter'] = parms['beam_diameter']*4  #  computational beam width at lens#1.  This matters!
+parms['lens1_fill_diameter'] = parms['beam_diameter']*5  #  computational beam width at lens#1.  This matters!
 parms['D_l1_to_pyr'] = parms['f1'] # nominal distance from lens1 to pyramid tip
 parms['D_l1_to_detector'] = 2*parms['f1']  # distance from lens to detector in 4f system
 parms['apex_diam'] = 40 # diameter of focal spot at pyramid apex in units of lambda_over_D (set by stop)
 parms['beam_diameter_at_pyramid'] = parms['apex_diam']*parms['f1']*parms['wavelength']/parms['beam_diameter']
 parms['detector_width'] = None # width of detector
-parms['max_chirp_step_deg'] = 30  # maximum allowed value (degrees) in chirp step for Fresnel prop
-parms['max_lens_step_deg'] = 20 # maximum step size allowed for lens phase screen
-parms['max_plane_step_deg'] = 20  # (degrees) maximum phase step allowed for planar phase screen
-parms['max_pyramid_phase_step'] = 20  # maximum step (degrees) allowed for pyramid phase ramp
+parms['max_chirp_step_deg'] = 10  # maximum allowed value (degrees) in chirp step for Fresnel prop
+parms['max_lens_step_deg'] = 10 # maximum step size allowed for lens phase screen
+parms['max_plane_step_deg'] = None  # (degrees) maximum phase step allowed for planar phase screen
+parms['max_pyramid_phase_step'] = 10  # maximum step (degrees) allowed for pyramid phase ramp
 parms['max_fractional_change'] = 1.e-5  # maximum change tolerated in certain finite differences
 parms['interp_style'] = 'linear'  # type of 2d interpolator for field resampling
 
@@ -182,7 +182,7 @@ def PropF4(u_start, x_start, params=parms, diagnostics=False):
         return({'x': x2d, 'field': field2d})
 
 
-MakeSysmatF4 = False
+MakeSysmatF4 = True
 if MakeSysmatF4:
     u = np.zeros((nppix,)).astype('complex')
     k=0; u[k] = 1.0 
@@ -200,3 +200,14 @@ if MakeSysmatF4:
         result = PropF4(uu, xu, params=parms, diagnostics=False)
         sysmat[:,k] = result['field'].reshape(nv*nv,)
     print('sysmat is done.')
+
+ReduceF4sysmat = False
+if ReduceF4sysmat:
+    nnx = 138
+    sm = np.zeros((nnx*nnx, nppix)).astype('complex')
+    for k in range(nppix):
+        v = sysmat[:,k].reshape(712,712)
+        vr = dc(v, 4, ftype='fir', axis=0, zero_phase=True)
+        vr = dc(vr, 4, ftype='fir',axis=1,zero_phase=True)
+        vr = vr[20:158,20:158]
+        sm[:,k] = vr.reshape(nnx*nnx,)
