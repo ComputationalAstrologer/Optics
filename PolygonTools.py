40a4270059737fc607d31fb51f13c4543bf4f195
ComputationalAstrologer <polygraph911@gmail.com>
2022-08-17 20:49:16 -0400

    neighbors still not work on HexagonTile

--- PolygonTools.py
+++ PolygonTools.py
@@ -9,12 +9,6 @@ Created on Thu Aug 27 16:06:24 2020
 import numpy as np
 
 
-nn = 100
-s = np.linspace(-1., 1., nn)
-[xx, yy] = np.meshgrid(s, s, indexing='xy')
-
-
-
 #This returns a 2D function that is 1 inside the polygon and 0 otherwise
 #mesh - tuple (two 2D arrays) output of np.meshgrid, e.g.,
 #   s = np.linspace(-1.1,1.1,243);  mesh = np.meshgrid(s,s);
@@ -80,3 +74,86 @@ def CutCircle(square_size, circ_diam, gtORgeq = 'gt'):
         mask[q[0][k], q[1][k]] = 0.
     return(mask)
 
+"""
+This is a class that handles various functions for a regular hexagon tiling.
+R - the radius of the circle that inscribes a hex segment
+EdgeWidth - the width (within a given segment) of the transition to the neighboring segment (same units as R!)
+  If set to 0, there is no transition.  If > 0, at the edge of the segment the height will be the 
+  mean of the segment and its neighbor
+Nx - the number of tiles in the x-direction
+Ny - the number of tiles in the y-direction
+"""
+
+
+class HexagonTile():
+    def __init__(self, R=1.0, EdgeWidth=0., Nx=3, Ny=3):
+        assert EdgeWidth >= 0.
+        self.R = R
+        self.ew = EdgeWidth
+        self.Nhex = Nx*Ny
+        self.Index = {'xy2lin': {}, 'lin2xy': {}}  # 2d and 1d index maps
+        self.Seg = {}  # info about individual segments, indexed by the values in self.Index['linind']
+        
+        #find the center for each hex in the grid
+        if np.mod(Nx, 2) > 0:
+            xind = np.arange(-(Nx-1)/2, (Nx-1)/2 + 1).astype('int')
+        else:
+            xind = np.arange(-Nx/2 + 1, Nx/2 + 1).astype('int')
+        if np.mod(Ny, 2) > 0:
+            yind = np.arange(-(Ny-1)/2, (Ny-1)/2 + 1).astype('int')
+        else:
+            yind = np.arange(-Ny/2 + 1, Ny/2 + 1).astype('int')
+        idval = -1
+        for kx in range(Nx):  # fill out the 'id' and 'revid' dicts
+            for ky in range(Ny):
+                idval += 1
+                self.Index['xy2lin'][(xind[kx], yind[ky])] = idval
+                self.Index['lin2xy'][idval] = (xind[kx], yind[ky])
+                self.Seg[idval] = dict()
+
+        for k in self.Index['lin2xy']:  # fill out the center values
+            xyind = self.Index['lin2xy'][k]
+            xcen = xyind[0]*3*R/2
+            if np.mod(xyind[0], 2) > 0:  # note: e.g., np.mod(-5,2) = 1
+                ycen = xyind[1]*3*R/2 + R
+            else:
+                ycen = xyind[1]*3*R/2
+            self.Seg[k]['center'] = (xcen, ycen)
+            self.Seg[k]['neighbors'] = np.zeros(6).astype('int')  # one neighbor for each sector
+
+        for k in self.Index['lin2xy']:
+            xyind = self.Index['lin2xy'][k]
+
+            for ks in range(6):  # loop over sectors
+                mx = xyind[0]
+                my = xyind[1]
+
+
+                if ks == 0:  # upper right
+                        mx += 1
+                        if np.mod(mx, 2) == 1:
+                            my += 1
+                elif ks == 1:  # above
+                        my += 1
+                elif ks == 2: # upper left
+                        mx -= 1
+                        if np.mod(mx, 2) == 1:
+                            my += 1
+                elif ks == 3: # lower left
+                        mx -= 1
+                        if np.mod(mx,2) == 0:
+                            my -= 1
+                elif ks == 4:  # below
+                        my -= 1
+                elif ks == 5:          # lower right
+                    mx += 1
+                    if np.mod(mx,2) == 0:
+                        my -= 1
+                else:
+                    print('case error');
+                    assert False
+                if (mx, my) in self.Index['xy2lin']:
+                    self.Seg[k]['neighbors'][ks] = self.Index['xy2lin'][(mx, my)]
+                else:  # no neighbor for that sector
+                    self.Seg[k]['neighbors'][ks] = -1  # no neighbor code
+        return(None)
