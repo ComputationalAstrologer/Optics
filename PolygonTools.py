f4a975f58685469066c4b838c8e3be66f693e7cc
ComputationalAstrologer <polygraph911@gmail.com>
2022-08-19 19:24:59 -0400

    Shapes that show up on PlotIndexMap don't make sense.  Don't know why.

--- PolygonTools.py
+++ PolygonTools.py
@@ -7,6 +7,7 @@ Created on Thu Aug 27 16:06:24 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 #This returns a 2D function that is 1 inside the polygon and 0 otherwise
@@ -85,7 +86,7 @@ EdgeWidth - the width (within a given segment) of the transition to the neighbor
 HeightFile - filename containing height map.  If None, random heights will be assigned
 """
 
-
+#=======================Start HexagonTile Class================================
 class HexagonTile():
     def __init__(self, R=1.0, Nx=3, Ny=3, EdgeWidth=0., HeightFile=None):
         assert EdgeWidth >= 0. and EdgeWidth < np.sqrt(3)*R/2
@@ -95,7 +96,7 @@ class HexagonTile():
         self.HeightFile = HeightFile
         self.Index = {'xy2lin': {}, 'lin2xy': {}}  # 2d and 1d index maps
         self.Seg = {}  # info about individual segments, indexed by the values in self.Index['linind']
-        self.Rmax2 = 0.  # square of maximum radius to be on grid
+        self.Rmax = 0.  # maximum radius to be on grid
         
         #find the center for each hex in the grid
         if np.mod(Nx, 2) > 0:
@@ -122,13 +123,16 @@ class HexagonTile():
             else:
                 ycen = xyind[1]*3*R/2
             self.Seg[k]['center'] = (xcen, ycen)
-            if self.Rmax2 < xcen*xcen + ycen*ycen:
-                self.Rmax2 = xcen*xcen + ycen*ycen
-            self.Rmax2 = (self.R + np.sqrt(self.Rmax2))**2
+            if self.Rmax < np.sqrt(xcen*xcen + ycen*ycen):
+                self.Rmax = np.sqrt(xcen*xcen + ycen*ycen)
+            self.Rmax = self.R + self.Rmax
 
             #initialize the other dicts with info about each segment
             self.Seg[k]['neighbors'] = np.zeros(6).astype('int')  # one neighbor for each sector
+            self.Seg[k]['border'] = False  # true for border pixels at edges of the mask
             self.Seg[k]['height'] = 0.  # height of the segment (excluding the edge)
+
+        self.GetHexHeights()  # get the height of each segment
 
         #fill out the neighbors dict
         for k in self.Index['lin2xy']:
@@ -160,27 +164,56 @@ class HexagonTile():
                      self.Seg[k]['neighbors'][ks] = self.Index['xy2lin'][(mx, my)]
                 else:  # no neighbor for that sector
                      self.Seg[k]['neighbors'][ks] = -1  # no neighbor code
+                     self.Seg[k]['border'] = True  # if it has less than 6 neighbors, it is a border pixel
         return(None)
 
     #This calculates the height corresponding to a given point, including the edge
     #point - any 2-element array-like object, e.g., (x,y), [x,y], where x and y
     #  specify the point's location.  Same units as the 'R' and 'EdgeWidth' 
-    #  arguments of the constructor 
+    #  arguments of the constructor
+    #Unless a point is closest to the center of a border segment, it is inside the segment
+    #  with the closest center (due to convexity)
     #I WILL VECTORIZE THIS LATER
     def CalcPointHeight(self, point):
+        hs = self.R*np.sqrt(3)/2  # distance from segment center to a side
+        height = 0.
         pt = np.array(point)
-        if pt[0]**2 + pt[1]**2 >= self.Rmax2:  height = 0.
+        if np.sqrt(pt[0]**2 + pt[1]**2) >= self.Rmax:  # point is outside the grid
+            return(height)
         #find the segment center to which pt is closest
         segid = -1  #
-        d2 = self.Rmax2
+        d2 = self.Rmax
         for k in self.Index['lin2xy']:
             ct = self.Seg[k]['center']
-            dt2 = (pt[0] - ct[0])**2 + (pt[1] - ct[1])**2
+            dt2 = np.sqrt((pt[0] - ct[0])**2 + (pt[1] - ct[1])**2)
             if dt2 < d2: 
                 d2 = dt2
-                segid = k
+                segid = k  # the point is inside segment sedig, unless segment segid is on the border
+
+        #first, figure out which sector it's in - needed for border pixels and treating edges
+        xc = pt[0] - self.Seg[segid]['center'][0]
+        yc = pt[1] - self.Seg[segid]['center'][1]
+        dc = np.sqrt(xc*xc + yc*yc)
+        ang = np.arctan2(yc, xc)
+        if ang < 0: ang += 2*np.pi
+        sector = np.floor( ang/(np.pi/3) ).astype('int')
         
-        
+        #now determine where the point is in relationship to the sector boundary
+        cbisang = np.cos(ang - sector*np.pi/3 - np.pi/6)  # cosine of the angle between center-point line and sector bisector line
+        dproj = dc*cbisang  # projected distance along sector bisector
+        if dproj > hs:
+            height = 0.  # point is outside of segment, which only happens on border segments because segid is the one closest to the point
+        elif dproj <= (hs - self.ew):  # point is in interior of segment
+            return(self.Seg[segid]['height'])
+        else: #complicated edge region case!
+            nabe = self.Seg[segid]['neighbors'][sector]
+            dq = dproj - (hs - self.ew)
+            if nabe == -1:  # no neighbor
+                nheight = 0.
+            else:
+                nheight = self.Seg[nabe]['height']  # height of neighbor
+                mheight = 0.5*(nheight + self.Seg[segid]['height'])  # height at sector boundary
+                height = self.Seg[segid]['height'] + (dq/self.ew)*(mheight - self.Seg[segid]['height'])
         return(height)
 
 
@@ -188,9 +221,37 @@ class HexagonTile():
     def GetHexHeights(self):
         if self.HeightFile is None:  # fill this out randomly
             for k in self.Index['lin2xy']:
-                self.Seg[k]['height'] = np.random.randn()
+                self.Seg[k]['height'] = np.random.rand()
         else:
             print('HeightFile kwarg under construction!')
             assert False
         return(None)
 
+    #This debugging code plots the hex index numbers on a 2D grid.
+    def PlotIndexMap(self):
+        plt.figure();
+        for k in self.Index['lin2xy']:
+            c = self.Seg[k]['center']
+            plt.plot(c[0],c[1],'rh')
+        for k in self.Index['lin2xy']:
+            c = self.Seg[k]['center']
+            plt.text(c[0], c[1], str(k))
+        return(None)
+
+#================= end of HexagonTile Class  ==================================
+
+#This provides an example of using the HexagonTile Class
+def HexTileExample(N=5, R=1/3):  # full width of grid is about 2*R*N
+    EdgeWidth = 0.
+    HT =  HexagonTile(R=R, Nx=N, Ny=N, EdgeWidth=EdgeWidth, HeightFile=None)
+    s = np.linspace(-1.1*R*N, 1.1*R*N, 100*N)
+    ext = [min(s), max(s), min(s), max(s)]
+    (xx, yy) = np.meshgrid(s,-s)
+    hmap = np.zeros(xx.shape)
+    for kx in range(xx.shape[0]):
+        for ky in range(xx.shape[1]):
+            hmap[kx,ky] = HT.CalcPointHeight((xx[kx,ky], yy[kx, ky]))
+    plt.figure();
+    plt.imshow(hmap, extent=ext); plt.colorbar(); plt.xlabel('x'); plt.ylabel('y'); plt.title('HexagonTile Example (height map)');
+    return((HT, hmap))
+    
