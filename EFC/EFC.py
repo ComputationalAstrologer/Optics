a7cc9fe8a970b123db8e70da0425812b12d06f36
ComputationalAstrologer <polygraph911@gmail.com>
2024-03-15 20:48:31 -0400

    centered dark hole works

--- EFC/EFC.py
+++ EFC/EFC.py
@@ -11,34 +11,36 @@ In this simple model, the polarized detector fields are given by a spline coeffi
 """
 
 import numpy as np
-from os import path as ospath
-from sys import path
+from os import path as ospath  #needed for isfile(), join(), etc.
+from sys import path as syspath
 from scipy import optimize
 import matplotlib.pyplot as plt
-#machine = "homeLinux"
-machine = "officeWindows"
+machine = "homeLinux"
+#machine = "officeWindows"
 if machine == "homeLinux":
     MySplineToolsLocation = "/home/rfrazin/Py/Optics"
     PropMatLoc = "/home/rfrazin/Py/EFCSimData/"
 elif machine == 'officeWindows':
+    assert False  # not setup correctly for pathinsert() below
     MySplineToolsLocation = "E:/Python/Optics"
     PropMatLoc = "E:/MyOpticalSetups/EFC\ Papers/DataArrays"
-path.insert(0, MySplineToolsLocation)
+syspath.insert(0, MySplineToolsLocation)
 import Bspline3 as BS  # this module is in MySplineToolsLocation
 
-HoleBndy = np.array([227, 285, 314, 372])
-SysXfn = 'SysMat_LgOAPcg21x21_ContrUnits_Ex.npy'
-SysYfn = 'SysMat_LgOAPcg21x21_ContrUnits_Ey.npy'
+HoleBndy = np.array([227, 285, 314, 372])  #[Xmin, Xmax,Ymin,Ymax]  on 512x512
+Reduced = True
+
+Sxfn = 'SyMat_LgOAPcg21x21_ContrUnits_Ex.npy'
+Syfn = 'SyMat_LgOAPcg21x21_ContrUnits_Ey.npy'
 SpecfieldXfn = 'SpeckleFieldFrom24x24screen_Ex.npy'
 SpecfieldYfn = 'SpeckleFieldFrom24x24screen_Ey.npy'
 fpsize = 512  # size of focal plane in pixels
 fplength = 20. #length of detector in mm
-Reduced = True
 if Reduced:
     fpsize //= 2
     HoleBndy //=2 
-    SysXfn = 'SysMatReduced_LgOAPcg21x21_ContrUnits_Ex.npy'
-    SysYfn = 'SysMatReduced_LgOAPcg21x21_ContrUnits_Ey.npy'
+    Sxfn = 'SysMatReduced_LgOAPcg21x21_ContrUnits_Ex.npy'
+    Syfn = 'SysMatReduced_LgOAPcg21x21_ContrUnits_Ey.npy'
     SpecfieldXfn = 'SpeckleFieldReducedFrom24x24screen_Ex.npy'
     SpecfieldYfn = 'SpeckleFieldReducedFrom24x24screen_Ey.npy'
 
@@ -58,73 +60,74 @@ class EFC():
         self.ndm = 21  # number of actuators (1D)
         self.lamdpix = (fpsize/fplength)*5*800*(self.lamb*1.e-3)/(21*0.3367) # "lambda/D" in pixel units, i.e., (pixels per mm)*magnification*focal length*lambda/diameter
         self.SpeckleFactor = SpeckleFactor  # see self.PolIntensity.  This can be changed in its function call
-        SysX = np.load(PropMatLoc + SysXfn )  # system matrices
-        SysY = np.load(PropMatLoc + SysYfn )
-        SpecX = np.load(PropMatLoc + SpecfieldXfn )  # X polarized speckles
-        SpecY = np.load(PropMatLoc + SpecfieldYfn)  # y polarized speckles
-        assert SysX.shape[1] == self.ndm**2
-        assert SysY.shape == SysX.shape
+        self.Sx = np.load(PropMatLoc + Sxfn )  # Sytem matrices
+        self.Sy = np.load(PropMatLoc + Syfn )
+        self.Spx = np.load(PropMatLoc + SpecfieldXfn )  # X polarized speckles
+        self.Spy = np.load(PropMatLoc + SpecfieldYfn)  # y polarized speckles
+        assert self.Sx.shape[1] == self.ndm**2
+        assert self.Sy.shape == self.Sx.shape
         self.HoleBndy = HoleBndy
         
         if HoleBndy is not None:  #trim the matrices and the speckle field to correspond to HoleBndy
-            sz = int(np.sqrt(SysX.shape[0]))
-            self.lindex = []  # 1D pixel index of dark hole pixel
-            self.duodex = []  # 2D pixel index of dark hole pixels
-            self.SpecX = SpecX[HoleBndy[2]:HoleBndy[3] + 1, HoleBndy[0]:HoleBndy[1] +1]
-            self.SpecY = SpecY[HoleBndy[2]:HoleBndy[3] + 1, HoleBndy[0]:HoleBndy[1] +1]
-            for row in np.arange(HoleBndy[0], HoleBndy[1] + 1):
-                for col in np.arange(HoleBndy[2], HoleBndy[3] +1):
-                   self.duodex.append( (row,col) )
-                   self.lindex.append( np.ravel_multi_index((row,col), (sz,sz)) )
-            self.SysX = np.zeros((len(self.lindex), self.ndm**2)).astype('complex')
-            self.SysY = np.zeros((len(self.lindex), self.ndm**2)).astype('complex')
-            for k in range(len(self.lindex)):
-                self.SysX[k,:] = SysX[self.lindex[k],:]
-                self.SysY[k,:] = SysY[self.lindex[k],:]
-        else:
-            self.SpecX = SpecX
-            self.SpecY = SpecY
-            self.SysX = SysX
-            self.SysY = SysY
-         
+            sz = int(np.sqrt(self.Sx.shape[0]))
+            lindex = []  # 1D pixel index of dark hole pixel
+            self.SpHx = self.Spx[HoleBndy[2]:HoleBndy[3] + 1, HoleBndy[0]:HoleBndy[1] +1]
+            self.SpHy = self.Spy[HoleBndy[2]:HoleBndy[3] + 1, HoleBndy[0]:HoleBndy[1] +1]
+            for col in np.arange(HoleBndy[0], HoleBndy[1] + 1):  # range in X pixels
+                for row in np.arange(HoleBndy[2], HoleBndy[3] +1):  # range in Y pixels
+                   lindex.append( np.ravel_multi_index((row,col), (sz,sz)) )
+            assert len(lindex) == self.HoleShape[0]*self.HoleShape[1]
+            self.Shx = np.zeros((len(lindex), self.ndm**2)).astype('complex')
+            self.Shy = np.zeros((len(lindex), self.ndm**2)).astype('complex')
+            for k in range(len(lindex)):
+                self.Shx[k,:] = self.Sx[lindex[k],:]
+                self.Shy[k,:] = self.Sy[lindex[k],:]    
+        else: pass
         return(None)
     
     #This returns the x- or y- polarized intensity as a function of the spline coefficient vector
     #The spline coefficient vector must have a shape (self.ndm**2,)
     #XorY - select the desired polarization 'X' or 'Y'
+    #region - if 'Hole' only the intensity inside self.HoleBndy is computed.
+    #            'Full' the intensity is calculated over the entire range
     #DMheight - if True the coefficient vector is interpreted as a DM height in microns and must be real-valued
     #          - if False it is simply a coefficient and can be complex valued
     #return_grad - return the gradient.  For now, only works when DMheight is True
     #SpeckleFactor - multiplier for additive speckle field.  Can be 0.  None corresponds to defaul (see __init__)
-    def PolIntensity(self, coef, XorY='X', DMheight=True, return_grad=True,
+    def PolIntensity(self, coef, XorY='X', region='Hole', DMheight=True, return_grad=True,
                      SpeckleFactor=None):
+        assert region == 'Hole' or region == 'Full'
         nc = self.ndm**2
         if SpeckleFactor is None: SpeckleFactor = self.SpeckleFactor
         assert coef.shape == (nc,)
         assert XorY == 'X' or XorY == 'Y'
         if XorY == 'X': 
-            Sys = self.SysX
-            sp  = self.SpecY
+            Sys = self.Sx
+            sp  = self.Spx
+            if region == 'Hole':
+                Sys = self.Shx
+                sp = self.SpHx
         else:
-            Sys = self.SysY
-            sp  = self.SpecY
+            Sys = self.Sy
+            sp  = self.Spy
+            if region == 'Hole':
+                Sys = self.Shy
+                sp  = self.SpHy
         if DMheight:
             assert np.iscomplexobj(coef) == False
-            c = np.exp(2j*np.pi*coef/self.lamb)
+            c = np.exp(1j*4.*np.pi*coef/self.lamb)
             if return_grad:
-                dc = 2j*np.pi*c/self.lamb
+                dc = 1j*4.*np.pi*c/self.lamb
         else:
             c = 1.0*coef
             if return_grad:
-                print('No gradient information provided when DMheight is False.')
-                assert False
-
+                dc = np.ones(coef.shape).astype('complex')*1.0
         f = Sys.dot(c)
         f += SpeckleFactor*sp.reshape(f.shape)
         I = np.real(f*np.conj(f))
         if not return_grad:
             return I
-        df = Sys*dc # speckles don't depend on c in this approximation.  This is the same as Sys.dot(diag(dc))
+        df = Sys*dc # speckles don't depend on c in this approximation.  This is the same as Sy.dot(diag(dc))
         dI = 2*np.real(np.conj(f)*df.T).T
         return (I, dI)
     
@@ -132,52 +135,17 @@ class EFC():
     #c - DM command vector
     def CostHoleDominant(self, c, return_grad=True):
         assert np.iscomplexobj(c) is False
-        assert c.shape == (self.SysX.shape[1],)
+        assert c.shape == (self.Sx.shape[1],)
         if return_grad:
-            I, dI = self.PolIntensity(c,XorY='X',DMheight=True,return_grad=True)
+            I, dI = self.PolIntensity(c,XorY='X',region='Hole',DMheight=True,return_grad=True)
             cost = np.sum(I)
             dcost = np.sum(dI, axis=0)
             return (cost, dcost)
         else:
-            I     = self.PolIntensity(c,XorY='X',DMheight=True,return_grad=False)
+            I     = self.PolIntensity(c,XorY='X',region='Hole',DMheight=True,return_grad=False)
             cost = np.sum(I)
             return cost
-    
-    #This calculates something like signal-to-noise ratio for the cross polarization
-    #metric - the various options correspond to:
-    #  'A' - Iy/(const + Ix)
-    def CostCrossSNR(self,c, metric='A'):  # passed gradient test 2/5/24
-        const = 1.e-9
-        Ix, gIx = self.PolIntensity(c,'X')
-        Iy, gIy = self.PolIntensity(c,'Y',SpeckleFactor=0.)
-        if metric == 'A':
-            s = np.sum(Iy/(const + Ix))
-            gs = gIy.T/(const + Ix) - gIx.T*Iy/( (const + Ix)**2 )
-            gs = np.sum(gs,axis=1)
-        else: assert False
-        return (-1*s, -1*gs)  #make it loss function instead of a profit function
-    
-    #This uses the Newton Conjugate Grad method to find minima of a function
-    #   from a number of random starting points.
-    def FindCrossMinima(self):
-        funB = self.CostCrossSNR
-        options={'disp':False, 'maxiter':15}
-        rnd = lambda scale : scale*(2*np.pi*np.random.rand(self.ndm**2) - np.pi)
-        nholes = 9
-        command_dh = []
-        cost_dh = []
-        command_c = []
-        cost_c = []
-        for k in range(nholes):
-          out = optimize.minimize(self.CostHoleDominant,rnd(1.),args=(),method='Newton-CG',options=options,jac=True)
-          command_dh.append(out['x'])
-          cost_dh.append(out['fun'])
-          out = optimize.minimize(self.CostCrossSNR,out['x'],args=(),method='Newton-CG',options=options,jac=True)
-          command_c.append(out['x'])
-          cost_c.append(out['fun'])
-          
-        return {'dh_commands': command_dh, 'cross_commands': command_c, 'dh_cost': cost_dh, 'cross_cost': cost_c}
-    
+        
     #This does the optimization over the dominant intensity to dig the dark hole
     #c0 - initial guess
     #method - options are 'NCG' (Newton Conj Grad), 'SLSQP'
@@ -196,3 +164,39 @@ class EFC():
                                     method='SLSQP',jac=True,constraints=(constr,))
         return out
     
+    #This calculates something like signal-to-noise ratio for the cross polarization
+    #metric - the various options correspond to:
+    #  'A' - Iy/(const + Ix)
+    def _CostCrossSNR(self,c, metric='A'):  # passed gradient test 2/5/24
+        assert False # this approach doesn't seem to work
+        const = 1.e-9
+        Ix, gIx = self.PolIntensity(c,'X')
+        Iy, gIy = self.PolIntensity(c,'Y',SpeckleFactor=0.)
+        if metric == 'A':
+            s = np.sum(Iy/(const + Ix))
+            gs = gIy.T/(const + Ix) - gIx.T*Iy/( (const + Ix)**2 )
+            gs = np.sum(gs,axis=1)
+        else: assert False
+        return (-1*s, -1*gs)  #make it loss function instead of a profit function
+    
+    #This uses the Newton Conjugate Grad method to find minima of a function
+    #   from a number of random starting points.
+    def _FindCrossMinima(self):
+        assert False  # this approach doesn't seem to work
+        funB = self._CostCrossSNR
+        options={'disp':False, 'maxiter':15}
+        rnd = lambda scale : scale*(2*np.pi*np.random.rand(self.ndm**2) - np.pi)
+        nholes = 9
+        command_dh = []
+        cost_dh = []
+        command_c = []
+        cost_c = []
+        for k in range(nholes):
+          out = optimize.minimize(self.CostHoleDominant,rnd(1.),args=(),method='Newton-CG',options=options,jac=True)
+          command_dh.append(out['x'])
+          cost_dh.append(out['fun'])
+          out = optimize.minimize(self.CostCrossSNR,out['x'],args=(),method='Newton-CG',options=options,jac=True)
+          command_c.append(out['x'])
+          cost_c.append(out['fun'])
+          
+        return {'dh_commands': command_dh, 'cross_commands': command_c, 'dh_cost': cost_dh, 'cross_cost': cost_c}
