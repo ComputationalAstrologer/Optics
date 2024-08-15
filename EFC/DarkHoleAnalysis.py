a24855c978c9b3e19f1964f00d46fc3c2efeaacb
ComputationalAstrologer <polygraph911@gmail.com>
2024-08-15 18:23:31 -0400

    ...

--- EFC/DarkHoleAnalysis.py
+++ EFC/DarkHoleAnalysis.py
@@ -5,7 +5,7 @@ Created on Tue Apr  9 18:09:24 2024
 @author: rfrazin
 
 This provides analysis based on EFC class in EFC.py. 
-It's kind of a grabbag of random crap
+It's kind of a grabbag of code lines
 
 """
 
@@ -21,19 +21,15 @@ CRB_Poisson = EFC.CRB_Poisson
 EFC = EFC.EFC  # need this to load the pickle
 
 #get EFC instance w/ dark hole command
-stuff = pickle.load(open('minus10HoleWithSpeckles33x33.pickle','rb'))
-Z = stuff['EFC class'];
+with open('minus10HoleWithSpeckles33x33.pickle','rb') as filep: stuff = pickle.load(filep);
 Cdh = stuff['DH command']  # Dark Hole command (phase values not DM height)
+#Z = stuff['EFC class'];
 
+extfac = np.sqrt(1.e-5) # linear polarizer (amplitude) extinction factor
 
 ######################################################################
 # optimization to find good probes (assuming 10^-5 linear polarizer)
 ######################################################################
-
-extfac = np.sqrt(1.e-5) # linear polarizer (amplitude) extinction factor
-
-lgabsr = lambda a: np.log10(np.abs(np.real(a)))
-lgabsi = lambda a: np.log10(np.abs(np.imag(a)))
 
 IZ0fx = Z.PolIntensity(Cdh,XorY='X',region='Full',DM_mode='phase',return_grad=False,SpeckleFactor=None)
 IZ0fx = IZ0fx.reshape((256,256))
@@ -58,7 +54,7 @@ cost = cost_re + cost_im  # '+' concats two lists here!
 # load some optimized probe solutions and analyze the CRBs  #
 #############################################################
 from EFC import CRB_Poisson
-stuff = pickle.load(open('stuff20240626.pickle','rb'))
+with open('stuff20240626.pickle','rb') as filep: stuff = pickle.load(filep)
 A =stuff['EFCobj']; # EFC class object
 sols=stuff['solutions'];  # DM command solutions
 sIp = 0.003*stuff['sIp']; # sqrt (DOM intensity) for each solution (with sqrt(10^-5) polarizer)
@@ -97,28 +93,65 @@ metric = np.array(metric)
 met2 = np.max(metric,axis=1); b = np.where(met2 == met2.min())[0][0]
 sol1 = sols[k1k2[b][0]]; sol2 = sols[k1k2[b][1]]  # best solution 
 
-##### plot some stuff for the DM commands sol1 and sol2
-extfac = np.sqrt(1.e-6)
-pm = 1.
+#################################################################
+# load pickle containing the two best probes and do some tests  #
+#################################################################
+
+CSQ = lambda a: np.real( a*np.conj(a) )
+
+with open('minus10HoleWithSpeckles33x33.pickle','rb') as filep:
+  stuff = pickle.load(filep)
+Z = stuff['EFC class']  #  EFC class object  -  with aberrations
+Cdh = stuff['DH command']  # Dark Hole command (phase values not DM height)  2116 pixels in dark hole
+with open('stuff20240626.pickle','rb') as filep:
+  stuff= pickle.load(filep)
+A =stuff['EFCobj']; # EFC class object  -  no aberrations - 441 pixels in dark hole
+with open('TwoDMsolutions.pickle','rb') as filep:  # these are probe solutions found with a CRB analysis (see above)
+    stuff = pickle.load(filep)
+sol1 = stuff['solution1']; sol2 = stuff['solution2'];  del stuff
+
+#get the speckle fields from Z corresponding to the dark hole in A
+sphx = np.zeros((len(A.HolePixels),)).astype('complex')
+sphy = np.zeros((len(A.HolePixels),)).astype('complex')
+for k in range(len(A.HolePixels)):
+    sphx[k] = Z.spx[A.HolePixels[k]]  # dom speckles
+    sphy[k] = Z.spy[A.HolePixels[k]]  # cross speckles
+
+f0x = Z.Field(Cdh,'X','Hole','phase',False,None)  # unknown dom field
+f0y = Z.Field(Cdh,'Y','Hole','phase',False,None)  # unknown cross field
+fAhx0 = A.Field(Cdh,'X','Hole','phase',False,None);  # model field
+fAhy0 = A.Field(Cdh,'Y','Hole','phase',False,None);  # model field
+pm = 1
+px1 = A.Field(Cdh + pm*sol1,'X','Hole','phase',False,0.) - fAhx0;  # probe 1
+px2 = A.Field(Cdh + pm*sol2,'X','Hole','phase',False,0.) - fAhx0;  # probe 2
+py1 = A.Field(Cdh + pm*sol1,'Y','Hole','phase',False,0.) - fAhy0;  # probe 1
+py2 = A.Field(Cdh + pm*sol2,'Y','Hole','phase',False,0.) - fAhy0;  # probe 2
+
+photons = 1.e15; ifc = extfac**2
+S = np.zeros((len(A.HolePixels),2))  # array of true intensities
+U = 1.0*S  # array of measured intensities
+for k in range(len(A.HolePixels)):
+    S[k,0] = ifc*CSQ(f0x[k] + px1[k]) + CSQ(f0y[k] + py1[k])
+    S[k,1] = ifc*CSQ(f0x[k] + px2[k]) + CSQ(f0y[k] + py2[k])
+    U[k,0] = np.random.poisson(2 + photons*S[k,0])
+    U[k,1] = np.random.poisson(2 + photons*S[k,1])
+
+
+
+#############################################################
+#       plots for the DM commands sol1 and sol2             #
+#############################################################
+
+pm = 1.   # positive probes
 IAhx0 = A.PolIntensity(Cdh,'X','Hole','phase',False,None)
 fAhx0 = A.Field(Cdh,'X','Hole','phase',False,0.);
 fAhy0 = A.Field(Cdh,'Y','Hole','phase',False,0.);
 IAhx1 = A.PolIntensity(Cdh + pm*sol1,'X','Hole','phase',False,None)
 IAhx2 = A.PolIntensity(Cdh + pm*sol2,'X','Hole','phase',False,None)
-
-
-pm = 1.  #  positive probes
 phx1p = A.Field(Cdh + pm*sol1,'X','Hole','phase',False,0.) - fAhx0;
 phx2p = A.Field(Cdh + pm*sol2,'X','Hole','phase',False,0.) - fAhx0;
 phy1p = A.Field(Cdh + pm*sol1,'Y','Hole','phase',False,0.) - fAhy0;
 phy2p = A.Field(Cdh + pm*sol2,'Y','Hole','phase',False,0.) - fAhy0;
-
-
-pm = -1.  # negative probes
-phx1m = A.Field(Cdh + pm*sol1,'X','Hole','phase',False,0.) - fAhx0;
-phx2m = A.Field(Cdh + pm*sol2,'X','Hole','phase',False,0.) - fAhx0;
-phy1m = A.Field(Cdh + pm*sol1,'Y','Hole','phase',False,0.) - fAhy0;
-phy2m = A.Field(Cdh + pm*sol2,'Y','Hole','phase',False,0.) - fAhy0;
 
 
 plt.figure(); #result of modulation with solution 1
@@ -135,9 +168,20 @@ plt.plot(np.real(phy2p),marker='d',color='crimson',ls='None');
 plt.plot(np.imag(phy2p),marker='p',color='dodgerblue',ls='None');
 plt.title('Solution 2');plt.xlabel('pixel index'); plt.ylabel('modulation field')
 
+if False:
+  pm = -1.  # negative probes
+  phx1m = A.Field(Cdh + pm*sol1,'X','Hole','phase',False,0.) - fAhx0;
+  phx2m = A.Field(Cdh + pm*sol2,'X','Hole','phase',False,0.) - fAhx0;
+  phy1m = A.Field(Cdh + pm*sol1,'Y','Hole','phase',False,0.) - fAhy0;
+  phy2m = A.Field(Cdh + pm*sol2,'Y','Hole','phase',False,0.) - fAhy0;
+
+  plt.figure(); # fields w/o modulation
+  plt.plot(np.real(fAhy0),marker='d',color='crimson',ls='None');
+  plt.plot(np.imag(fAhy0),marker='p',color='dodgerblue',ls='None');
+  plt.title('Nominal Dark Hole Cross field');plt.xlabel('pixel index'); plt.ylabel('nominal field')
 
 
-#plt.figure(); # fields w/o modulation
-#plt.plot(np.real(fAhy0),marker='d',color='crimson',ls='None');
-#plt.plot(np.imag(fAhy0),marker='p',color='dodgerblue',ls='None');
-#plt.title('Nominal Dark Hole Cross field');plt.xlabel('pixel index'); plt.ylabel('nominal field')
+
+
+
+
